104feac3cc82e8ed1cee801defbeb76ab518e595
ivan <ipkozyrin@gmail.com>
2018-05-26 12:52:04 +1000

    refactor

---
+++ src/app/DeviceDispatcher.ts
@@ -0,0 +1,11 @@
+import App from './App';
+
+
+export default class DeviceDispatcher {
+  private readonly app: App;
+
+  constructor(app) {
+    this.app = app;
+  }
+
+}
---
+++ src/devices/BinarySensor.ts
@@ -0,0 +1,11 @@
+import App from '../app/App';
+
+
+export default class Router {
+  private readonly app: App;
+
+  constructor(app) {
+    this.app = app;
+  }
+
+}
