087647436031e0697b8321ec42501a97d7b2d3b2
ivan <ipkozyrin@gmail.com>
2018-06-05 22:36:39 +1000

    refactor

---
+++ src/app/Connections.ts
@@ -0,0 +1,91 @@
+import Connection from "./interfaces/Connection";
+import I2cConnection from "../connections/I2cConnection";
+import Destination from "./interfaces/Destination";
+import * as _ from "lodash";
+
+
+export default class Connections {
+  private readonly connectionsParams: {[index: string]: Destination};
+  private readonly connections: { [index: string]: Connection } = {};
+  private readonly connectionTypes: object = {
+    i2c: I2cConnection,
+  };
+
+  constructor(connectionsParams: {[index: string]: Destination}) {
+    this.connectionsParams = connectionsParams;
+  }
+
+  init(): void {
+    // TODO: !!!
+  }
+
+  send(destination: Destination, payload: any): Promise<void> {
+    // TODO: !!!
+  }
+
+  listenIncome(handler: (payload: any) => void): void {
+    // TODO: !!!
+  }
+
+  off(handler: (payload: any) => void): void {
+    // TODO: !!!
+  }
+
+  /**
+   * Configure slave to slave and local connections.
+   */
+  private configureConnections() {
+    const connectionTypes = this.collectConnectionTypes(this.app.host.config.neighbors);
+
+    _.each(connectionTypes, (item: { type: string, bus: string }) => {
+      this.registerConnection(item);
+    });
+  }
+
+  private collectConnectionTypes(neighbors: {[index: string]: Destination}): Array<{ type: string, bus: string }> {
+    const result = {};
+
+    _.each(neighbors, (item: Destination) => {
+
+      // TODO: test
+
+      const connectionType = _.pick(item, 'type', 'bus');
+
+      result[this.generateConnectionId(connectionType)] = connectionType;
+    });
+
+    return _.map(result);
+  }
+
+  private registerConnection(connectionType: { type: string, bus: string }) {
+    const connectionId = this.generateConnectionId(connectionType);
+    const ConnectionClass = this.connectionTypes[connectionType.type];
+
+    this.connections[connectionId] = new ConnectionClass(this.app, connectionType);
+    this.connections[connectionId].init();
+  }
+
+  private getConnection(connectionParams: Destination): Connection {
+    const connectionId = this.generateConnectionId(connectionParams);
+
+    if (!this.connections[connectionId]) {
+      throw new Error(`Can't find connection "${connectionId}"`);
+    }
+
+    return this.connections[connectionId];
+  }
+
+  private listenToAllConnections(): void {
+    _.each(this.connections, (connection: Connection) => {
+
+      // TODO: add address
+
+      connection.listenIncome(this.handleIncomeMessages);
+    });
+  }
+
+  private generateConnectionId(connection: { type: string, bus: string }): string {
+    return [ connection.type, connection.bus ].join('-');
+  }
+
+}
--- src/app/Router.ts
+++ src/app/Router.ts
@@ -2,6 +2,7 @@ import * as _ from 'lodash';
 import * as EventEmitter from 'events';
 
 import App from './App';
+import Connections from './Connections';
 import RouterMessage from './interfaces/RouterMessage';
 import Connection from './interfaces/Connection';
 import Destination from './interfaces/Destination';
@@ -15,31 +16,38 @@ import I2cConnection from '../connections/I2cConnection';
  */
 export default class Router {
   private readonly app: App;
+  // TODO: rename
+  private readonly destinations: Connections;
   private readonly events: EventEmitter = new EventEmitter();
-  private readonly connections: { [index: string]: Connection } = {};
   private readonly eventName: string = 'msg';
-  private readonly connectionTypes: object = {
-    i2c: I2cConnection,
-  };
+  // private readonly connections: { [index: string]: Connection } = {};
+  // private readonly connectionTypes: object = {
+  //   i2c: I2cConnection,
+  // };
 
   constructor(app) {
     this.app = app;
+    this.destinations = new Connections(this.app.host.config.neighbors);
   }
 
   init(): void {
-    this.configureConnections();
-    this.listenToAllConnections();
+    this.destinations.init();
+    // this.configureConnections();
+    // this.listenToAllConnections();
+
+
   }
 
   async send(toHost: string, payload: any): Promise<void> {
     if (!toHost) throw new Error(`You have to specify a "toHost" param`);
+    if (toHost === this.app.host.id) throw new Error(`You can't send message to yourself`);
 
-    // local messages are forwarded to loop back
-    if (toHost === this.app.host.id) {
-      this.sendToLoopBack(payload);
-
-      return;
-    }
+    // // local messages are forwarded to loop back
+    // if (toHost === this.app.host.id) {
+    //   this.sendToLoopBack(payload);
+    //
+    //   return;
+    // }
 
     // TODO: ждать таймаут ответа - если не дождались - do reject
     // TODO: как-то нужно дождаться что сообщение было доставленно принимающей стороной
@@ -47,9 +55,12 @@ export default class Router {
     const routerMessage: RouterMessage = this.generateMessage(toHost, payload);
     const nextHostId: string = this.resolveNextHostId(routerMessage.route);
     const nextHostConnectionParams: Destination = this.resolveHostConnection(nextHostId);
-    const connection = this.getConnection(nextHostConnectionParams);
 
-    await connection.send(nextHostConnectionParams.address, routerMessage);
+    await this.destinations.send(nextHostConnectionParams, routerMessage);
+
+    // const connection = this.getConnection(nextHostConnectionParams);
+    //
+    // await connection.send(nextHostConnectionParams.address, routerMessage);
   }
 
   /**
@@ -63,58 +74,58 @@ export default class Router {
     this.events.removeListener(this.eventName, handler);
   }
 
-  /**
-   * Configure slave to slave and local connections.
-   */
-  private configureConnections() {
-    const connectionTypes = this.collectConnectionTypes(this.app.host.config.neighbors);
+  // /**
+  //  * Configure slave to slave and local connections.
+  //  */
+  // private configureConnections() {
+  //   const connectionTypes = this.collectConnectionTypes(this.app.host.config.neighbors);
+  //
+  //   _.each(connectionTypes, (item: { type: string, bus: string }) => {
+  //     this.registerConnection(item);
+  //   });
+  // }
 
-    _.each(connectionTypes, (item: { type: string, bus: string }) => {
-      this.registerConnection(item);
-    });
-  }
+  // private collectConnectionTypes(neighbors: {[index: string]: Destination}): Array<{ type: string, bus: string }> {
+  //   const result = {};
+  //
+  //   _.each(neighbors, (item: Destination) => {
+  //
+  //     // TODO: test
+  //
+  //     const connectionType = _.pick(item, 'type', 'bus');
+  //
+  //     result[this.generateConnectionId(connectionType)] = connectionType;
+  //   });
+  //
+  //   return _.map(result);
+  // }
 
-  private collectConnectionTypes(neighbors: {[index: string]: Destination}): Array<{ type: string, bus: string }> {
-    const result = {};
+  // private registerConnection(connectionType: { type: string, bus: string }) {
+  //   const connectionId = this.generateConnectionId(connectionType);
+  //   const ConnectionClass = this.connectionTypes[connectionType.type];
+  //
+  //   this.connections[connectionId] = new ConnectionClass(this.app, connectionType);
+  //   this.connections[connectionId].init();
+  // }
 
-    _.each(neighbors, (item: Destination) => {
+  // private getConnection(connectionParams: Destination): Connection {
+  //   const connectionId = this.generateConnectionId(connectionParams);
+  //
+  //   if (!this.connections[connectionId]) {
+  //     throw new Error(`Can't find connection "${connectionId}"`);
+  //   }
+  //
+  //   return this.connections[connectionId];
+  // }
 
-      // TODO: test
-
-      const connectionType = _.pick(item, 'type', 'bus');
-
-      result[this.generateConnectionId(connectionType)] = connectionType;
-    });
-
-    return _.map(result);
-  }
-
-  private registerConnection(connectionType: { type: string, bus: string }) {
-    const connectionId = this.generateConnectionId(connectionType);
-    const ConnectionClass = this.connectionTypes[connectionType.type];
-
-    this.connections[connectionId] = new ConnectionClass(this.app, connectionType);
-    this.connections[connectionId].init();
-  }
-
-  private getConnection(connectionParams: Destination): Connection {
-    const connectionId = this.generateConnectionId(connectionParams);
-
-    if (!this.connections[connectionId]) {
-      throw new Error(`Can't find connection "${connectionId}"`);
-    }
-
-    return this.connections[connectionId];
-  }
-
-  private listenToAllConnections(): void {
-    _.each(this.connections, (connection: Connection) => {
-
-      // TODO: add address
-
-      connection.listenIncome(this.handleIncomeMessages);
-    });
-  }
+  // private listenToAllConnections(): void {
+  //   _.each(this.connections, (connection: Connection) => {
+  //
+  //     // TODO: add address
+  //
+  //     connection.listenIncome(this.handleIncomeMessages);
+  //   });
+  // }
 
   /**
    * Try to find the next host after current.
@@ -186,12 +197,11 @@ export default class Router {
     };
   }
 
-  private sendToLoopBack(payload: any): void {
-    this.events.emit(this.eventName, payload);
-  }
+  // private sendToLoopBack(payload: any): void {
+  //   this.events.emit(this.eventName, payload);
+  // }
 
-  private generateConnectionId(connection: { type: string, bus: string }): string {
-    return [ connection.type, connection.bus ].join('-');
-  }
-
+  // private generateConnectionId(connection: { type: string, bus: string }): string {
+  //   return [ connection.type, connection.bus ].join('-');
+  // }
 }
