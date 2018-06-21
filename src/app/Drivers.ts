993996c67ed45bf10f34346ba0904c13884245ad
Kozyrin Ivan <ipkozyrin@gmail.com>
2018-06-21 10:32:43 +1000

    add

--- src/app/Drivers.ts
+++ src/app/Drivers.ts
@@ -1,15 +1,40 @@
 import System from './System';
 
 
+interface Driver {
+  init?: () => void;
+}
+
+
 export default class Drivers {
   private readonly system: System;
+  private readonly instances: Map<string, Driver> = new Map();
 
   constructor(system: System) {
     this.system = system;
   }
 
-  getDriver(driverName: string): any {
+  init(driversPaths: Map<string, string>, driversConfig: {[index: string]: object} = {}) {
+    driversPaths.forEach((driverPath: string, driverName: string) => {
+      const DriverClass = this._require(driverPath);
 
+      this.instances.set(driverName, new DriverClass(this, driversConfig[driverName]));
+    });
+
+    // initialize drivers
+    for(let [key, driver] of this.instances) {
+      if (driver.init) driver.init();
+    }
+  }
+
+  getDriver(driverName: string): Driver | undefined {
+    // TODO: вернуть тип возвращаемого драйвера
+    return this.instances.get(driverName);
+  }
+
+  // it needs for test purpose
+  _require(devicePath: string) {
+    return require(devicePath);
   }
 
 }
