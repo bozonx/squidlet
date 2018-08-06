84bf0cfe83e3d92229f39cd15f983395f6fbaec5
Kozyrin Ivan <ipkozyrin@gmail.com>
2018-08-06 10:32:05 +1000

    add

---
+++ src/devices/BinarySensor/BinarySensor.ts
@@ -0,0 +1,11 @@
+import System from '../../app/System';
+
+
+export default class BinarySensor {
+  private readonly system: System;
+
+  constructor(system: System) {
+    this.system = system;
+  }
+
+}
