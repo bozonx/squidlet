5a2f533eb6b1ec1f01ff64b2bd6ff0dcb55e90c8
Kozyrin Ivan <ipkozyrin@gmail.com>
2018-08-27 10:35:51 +1000

    change

--- builder/squidlet-rpi/index.ts
+++ builder/squidlet-rpi/index.ts
@@ -1,1 +1,13 @@
-import '../../host/src/index';
+import System from '../../host/src/app/System';
+
+
+async function init() {
+  const system = new System();
+
+  await system.start();
+}
+
+init()
+  .catch((err) => {
+    throw new Error(err);
+  });
