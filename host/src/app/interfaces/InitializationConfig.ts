93a23341eb2078a23ea3b4264322b3af45ae5ef1
Kozyrin Ivan <ipkozyrin@gmail.com>
2018-08-28 13:51:43 +1000

    change

--- host/src/app/interfaces/InitializationConfig.ts
+++ host/src/app/interfaces/InitializationConfig.ts
@@ -1,3 +1,22 @@
 export default interface InitializationConfig {
+  hostDirs: {
+    config: string;
+    devices: string;
+    drivers: string;
+    services: string;
+  };
+  fileNames: {
+    hostConfig: string;
+    manifest: string;
 
+    systemDrivers: string;
+    regularDrivers: string;
+
+    systemServices: string;
+    regularServices: string;
+
+    devicesDefinitions: string;
+    driversDefinitions: string;
+    servicesDefinitions: string;
+  };
 }
--- host/src/app/systemConfig.ts
+++ host/src/app/systemConfig.ts
@@ -10,32 +10,4 @@ export default {
     // custom data or state of user layer
     data: 'data',
   },
-  // dirs of host dir
-  hostDirs: {
-    config: 'config',
-    // there is devices are placed by deviceId
-    devices: 'devices',
-    drivers: 'drivers',
-    services: 'services',
-  },
-  fileNames: {
-    hostConfig: 'hostConfig.json',
-    manifest: 'manifest.json',
-    // list of system drivers like driverName[]
-    systemDrivers: 'systemDrivers.json',
-    // list of regular drivers like driverName[]
-    regularDrivers: 'regularDrivers.json',
-
-    // list of system services like serviceId[]
-    systemServices: 'systemServices.json',
-    // list of regular services like serviceId[]
-    regularServices: 'regularServices.json',
-
-    // definitions of devices like DeviceDefinition[]
-    devicesDefinitions: 'devicesDefinitions.json',
-    // definitions of drivers like {driverName: DriverDefinition}
-    driversDefinitions: 'driversDefinitions.json',
-    // definitions of services like ServiceDefinition[]
-    servicesDefinitions: 'servicesDefinitions.json',
-  },
 };
