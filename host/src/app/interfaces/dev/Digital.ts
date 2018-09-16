27c5e600094639920df31ce460fe6296a983b592
Ivan <ipkozyrin@gmail.com>
2018-09-17 09:06:44 +1000

    add

--- host/src/app/interfaces/dev/Digital.ts
+++ host/src/app/interfaces/dev/Digital.ts
@@ -1,8 +1,15 @@
-type WatchHandler = () => void;
+export type WatchHandler = () => void;
+// pin modes like on espruino
+export type PinMode = 'input'
+  | 'input_pullup'
+  | 'input_pulldown'
+  | 'output';
 
+
+// TODO: в digitalWrite - можно делать pulse если передать несколько значений
 
 export default interface Digital {
-  setup(pin: number): Promise<void>;
+  setup(pin: number, pinMode: PinMode): Promise<void>;
   read(pin: number): Promise<boolean>;
   write(pin: number, value: boolean): Promise<void>;
   watch(pin: number, handler: WatchHandler): void;
--- platforms/squidlet-esp32/dev/Digital.dev.ts
+++ platforms/squidlet-esp32/dev/Digital.dev.ts
@@ -1,19 +1,27 @@
-import Digital from '../../../host/src/app/interfaces/dev/Digital';
+import Digital, {PinMode, WatchHandler} from '../../../host/src/app/interfaces/dev/Digital';
+
+
+declare function digitalRead(pin: number): boolean;
+declare function digitalWrite(pin: number, value: boolean): void;
+declare function pinMode(pin: number, mode: string, automatic?: boolean): void;
+
 
 // TODO: проверить чтобы возвращался boolean
 // TODO: проверить чтобы принимался boolean
+// TODO: нужен ли automatic параметр в espruino ?
+
 
 export default class DigitalDev implements Digital {
-  setup(pin: number): Promise<void> {
-
+  async setup(pin: number, mode: PinMode): Promise<void> {
+    pinMode(pin, mode);
   }
 
-  read(pin: number): Promise<boolean> {
-
+  async read(pin: number): Promise<boolean> {
+    return digitalRead(pin);
   }
 
-  write(pin: number, value: boolean): Promise<void> {
-
+  async write(pin: number, value: boolean): Promise<void> {
+    digitalWrite(pin, value);
   }
 
   watch(pin: number, handler: WatchHandler): void {
