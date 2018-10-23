e2890f1bc5561fcb7588bf024da451470d47c945
Ivan <ipkozyrin@gmail.com>
2018-10-24 09:52:20 +1000

    add

---
+++ host/src/services/Mqtt/Mqtt.driver.ts
@@ -0,0 +1,9 @@
+import DriverBase from '../../app/entities/DriverBase';
+import {BinaryInputDriverProps} from '../../drivers/Binary/BinaryInput.driver';
+
+
+export class MqttDriver extends DriverBase<BinaryInputDriverProps> {
+
+}
+
+// TODO: нужны ли инстансы ???
---
+++ host/src/services/Mqtt/Mqtt.ts
@@ -0,0 +1,25 @@
+import ServiceBase from '../../app/entities/ServiceBase';
+import {GetDriverDep} from '../../app/entities/EntityBase';
+import {MqttDriver} from './Mqtt.driver';
+
+
+interface Props {
+
+}
+
+export default class Mqtt extends ServiceBase<Props> {
+  private get mqttDriver(): MqttDriver {
+    return this.depsInstances.digitalInput as MqttDriver;
+  }
+
+  protected willInit = async (getDriverDep: GetDriverDep) => {
+    this.depsInstances.mqttDriver = await getDriverDep('Mqtt.driver')
+      .getInstance(this.props);
+  }
+
+  // protected didInit = async () => {
+  //   this.digitalInput.addListener(this.listenHandler, this.props.debounce);
+  // }
+
+
+}
