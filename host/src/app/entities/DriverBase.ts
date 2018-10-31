183ca0fe4d94ce760ed5df61453eeec2faa2af3e
Ivan <ipkozyrin@gmail.com>
2018-10-31 10:13:39 +1000

    fix

--- host/src/app/entities/DriverBase.ts
+++ host/src/app/entities/DriverBase.ts
@@ -1,5 +1,13 @@
 import EntityBase from './EntityBase';
+import EntityDefinition from '../interfaces/EntityDefinition';
+import DriverEnv from './DriverEnv';
 
 
 export default class DriverBase<Props = {}> extends EntityBase<Props> {
+  protected readonly env: DriverEnv;
+
+  constructor(definition: EntityDefinition, env: DriverEnv) {
+    super(definition, env);
+    this.env = env;
+  }
 }
