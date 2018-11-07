aff9833ab1188d4d0fddb2e5ce9b4487e6222193
Ivan <ipkozyrin@gmail.com>
2018-11-07 10:12:32 +1000

    add

--- host/src/devices/BinaryClick/BinaryClick.ts
+++ host/src/devices/BinaryClick/BinaryClick.ts
@@ -1,47 +1,5 @@
-import DeviceBase, {DeviceBaseProps} from '../../baseDevice/DeviceBase';
-import {Data} from '../../baseDevice/DeviceDataManagerBase';
-import {DEFAULT_STATUS} from '../../baseDevice/Status';
-import {GetDriverDep} from '../../app/entities/EntityBase';
-import {BinaryInputDriver, BinaryInputDriverProps} from '../../drivers/Binary/BinaryInput.driver';
+import BinaryImpulse from '../BinaryImpulse/BinaryImpulse';
 
 
-interface Props extends DeviceBaseProps, BinaryInputDriverProps {
-}
-
-
-export default class BinaryClick extends DeviceBase<Props> {
-  private get binaryInput(): BinaryInputDriver {
-    return this.depsInstances.binaryInput as BinaryInputDriver;
-  }
-
-
-  protected willInit = async (getDriverDep: GetDriverDep) => {
-    this.depsInstances.binaryInput = await getDriverDep('BinaryInput.driver')
-      .getInstance(this.props);
-  }
-
-  protected didInit = async () => {
-    // listen driver's change and set status
-    this.binaryInput.addListener(this.onInputChange);
-  }
-
-
-  protected statusGetter = async (): Promise<Data> => {
-    return { [DEFAULT_STATUS]: await this.binaryInput.read() };
-  }
-
-  protected transformPublishValue = (value: boolean): number => {
-    return Number(value);
-  }
-
-  protected validateProps = (props: Props): string | undefined => {
-    // TODO: !!!! validate debounce and blockTime
-    return;
-  }
-
-
-  private onInputChange = async (level: boolean) => {
-    await this.setStatus(level);
-  }
-
+export default class BinaryClick extends BinaryImpulse {
 }
--- host/src/devices/BinarySwitch/BinarySwitch.ts
+++ host/src/devices/BinarySwitch/BinarySwitch.ts
@@ -1,7 +1,5 @@
 import BinarySensor from '../BinarySensor/BinarySensor';
 
 
-// TODO: только свое значение по умолчанию для таймаутов
-
 export default class BinarySwitch extends BinarySensor {
 }
