7f9ec3b99c1938d99a6879c84690baac1704e882
Ivan <ipkozyrin@gmail.com>
2018-12-01 10:30:40 +1000

    add

--- host/src/devices/Pcf8574/Pcf8574.ts
+++ host/src/devices/Pcf8574/Pcf8574.ts
@@ -2,41 +2,45 @@ import DeviceBase, {DeviceBaseProps} from '../../baseDevice/DeviceBase';
 import {Data} from '../../baseDevice/DeviceDataManagerBase';
 import {DEFAULT_STATUS} from '../../baseDevice/Status';
 import {GetDriverDep} from '../../app/entities/EntityBase';
-import {BinaryInputDriver, BinaryInputDriverProps} from '../../drivers/Binary/BinaryInput.driver';
+import {ExpanderDriverProps, PCF8574Driver} from '../../drivers/Pcf8574/Pcf8574.driver';
 
 
-interface Props extends DeviceBaseProps, BinaryInputDriverProps {
+interface Props extends DeviceBaseProps, ExpanderDriverProps {
 }
 
 
 export default class Pcf8574 extends DeviceBase<Props> {
-  private get binaryInput(): BinaryInputDriver {
-    return this.depsInstances.binaryInput as BinaryInputDriver;
+  private get expander(): PCF8574Driver {
+    return this.depsInstances.expander as PCF8574Driver;
   }
 
+  // TODO: отключить republish
+  // TODO: желательно проинициализировать одним запросом
+  // TODO: может по умолчанию статус отключить
+  // TODO: ??? навешаться на события
 
   protected willInit = async (getDriverDep: GetDriverDep) => {
-    this.depsInstances.binaryInput = await getDriverDep('Pcf8574.driver')
+    this.depsInstances.expander = await getDriverDep('Pcf8574.driver')
       .getInstance(this.props);
   }
 
   protected didInit = async () => {
     // listen driver's change
-    this.binaryInput.addListener(this.onInputChange);
+    //this.binaryInput.addListener(this.onInputChange);
   }
 
 
-  protected statusGetter = async (): Promise<Data> => {
-    return { [DEFAULT_STATUS]: await this.binaryInput.read() };
-  }
-
-  protected transformPublishValue = (value: number): string => {
-    return value.toString(16);
-  }
-
-
-  private onInputChange = async (level: boolean) => {
-    await this.setStatus(level);
-  }
+  // protected statusGetter = async (): Promise<Data> => {
+  //   return { [DEFAULT_STATUS]: await this.binaryInput.read() };
+  // }
+  //
+  // protected transformPublishValue = (binArr: number[]): string => {
+  //   return binArr.join('');
+  // }
+  //
+  //
+  // private onInputChange = async (binArr: number[]) => {
+  //   await this.setStatus(binArr);
+  // }
 
 }
