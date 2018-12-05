27821a39e60def6e5028c697bc9ebd255676a3a0
Ivan <ipkozyrin@gmail.com>
2018-12-06 09:55:24 +1000

    change messenger

--- host/src/messenger/Messenger.ts
+++ host/src/messenger/Messenger.ts
@@ -38,6 +38,11 @@ export default class Messenger {
     this.bridgeResponder.init();
   }
 
+
+  isLocalHost(toHost?: string) {
+    return !toHost || toHost === this.system.host.id;
+  }
+
   /**
    * Emit local message (send local)
    */
@@ -58,11 +63,11 @@ export default class Messenger {
    * Listen to events of current or remote host.
    * If toHost isn't equal to current host - it will subscribe to events of remote host.
    */
-  subscribeLocal(category: string, topic: string, handler: Handler): number {
+  subscribeLocal(category: string, topic: string, handler: Handler): string {
     return this.subscribe(this.system.host.id, category, topic, handler);
   }
 
-  subscribeCategoryLocal(category: string, handler: Handler): number {
+  subscribeCategoryLocal(category: string, handler: Handler): string {
     return this.subscribeCategory(this.system.host.id, category, handler);
   }
 
@@ -70,7 +75,7 @@ export default class Messenger {
    * Listen to events of current or remote host.
    * If toHost isn't equal to current host - it will subscribe to events of remote host.
    */
-  subscribe(toHost: string, category: string, topic: string, handler: Handler): number {
+  subscribe(toHost: string, category: string, topic: string, handler: Handler): string {
     if (!category) {
       throw new Error(`You have to specify the category`);
     }
@@ -82,16 +87,11 @@ export default class Messenger {
       handler(message && message.payload, message);
     };
 
-    if (this.isLocalHost(toHost)) {
-      // subscribe to local events
-      return this.system.events.addListener(category, topic, wrapper);
-    }
-
     // else subscribe to remote host's events
     return this.bridgeSubscriber.subscribe(toHost, category, topic, wrapper);
   }
 
-  subscribeCategory(toHost: string, category: string, handler: Handler): number {
+  subscribeCategory(toHost: string, category: string, handler: Handler): string {
     if (!category) {
       throw new Error(`You have to specify the category`);
     }
@@ -99,11 +99,6 @@ export default class Messenger {
     const wrapper = (message: Message) => {
       handler(message && message.payload, message);
     };
-
-    if (this.isLocalHost(toHost)) {
-      // subscribe to local events
-      return this.system.events.addCategoryListener(category, wrapper);
-    }
 
     // else subscribe to remote host's events
     return this.bridgeSubscriber.subscribeCategory(toHost, category, wrapper);
@@ -113,7 +108,7 @@ export default class Messenger {
    * Unsubscribe from events of remote or local host.
    * Handler has to be the same as has been specified to "subscribe" method previously.
    */
-  unsubscribe(toHost: string, category: string, topic: string, handlerId: number): void {
+  unsubscribe(toHost: string, category: string, topic: string, handlerId: string): void {
     if (!category) {
       throw new Error(`You have to specify the category`);
     }
@@ -121,29 +116,17 @@ export default class Messenger {
       throw new Error(`You have to specify the topic`);
     }
 
-    if (this.isLocalHost(toHost)) {
-      // subscribe to local events
-      this.system.events.removeListener(category, topic, handlerId);
-    }
-    else {
-      // unsubscribe from remote host's events
-      this.bridgeSubscriber.unsubscribe(toHost, category, topic, handlerId);
-    }
+    // unsubscribe from remote host's events
+    this.bridgeSubscriber.unsubscribe(toHost, category, topic, handlerId);
   }
 
-  unsubscribeCategory(toHost: string, category: string, handlerId: number): void {
+  unsubscribeCategory(toHost: string, category: string, handlerId: string): void {
     if (!category) {
       throw new Error(`You have to specify the category`);
     }
 
-    if (this.isLocalHost(toHost)) {
-      // subscribe to local events
-      this.system.events.removeCategoryListener(category, handlerId);
-    }
-    else {
-      // unsubscribe from remote host's events
-      this.bridgeSubscriber.unsubscribeCategory(toHost, category, handlerId);
-    }
+    // unsubscribe from remote host's events
+    this.bridgeSubscriber.unsubscribeCategory(toHost, category, handlerId);
   }
 
 
@@ -159,13 +142,15 @@ export default class Messenger {
   }
 
   listenRequests(topic: string, handler: (payload: any) => void): number {
+
+    // TODO: review
+
     return this.requestResponse.listenRequests(topic, handler);
   }
 
   removeRequestsListener(topic: string, handlerId: number): void {
     this.requestResponse.removeRequestsListener(topic, handlerId);
   }
-
 
   async $sendMessage(message: Message | Request): Promise<void> {
     // if message is addressed to local host - rise it immediately
@@ -176,11 +161,6 @@ export default class Messenger {
     }
 
     await this.system.network.send(message.to, message);
-  }
-
-
-  private isLocalHost(toHost?: string) {
-    return !toHost || toHost === this.system.host.id;
   }
 
 }
