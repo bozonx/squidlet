58b961d9bc5bda218a2269c19bcc544e9cd52c70
Ivan <ipkozyrin@gmail.com>
2018-12-06 10:45:34 +1000

    change BridgeSubscriber

--- host/src/messenger/BridgeSubscriber.ts
+++ host/src/messenger/BridgeSubscriber.ts
@@ -1,7 +1,6 @@
-import {ALL_TOPICS} from '../app/dict/constants';
-
 const _find = require('lodash/find');
 
+import {ALL_TOPICS} from '../app/dict/constants';
 import categories from '../app/dict/categories';
 import System from '../app/System';
 import Messenger from './Messenger';
@@ -9,17 +8,19 @@ import Message from './interfaces/Message';
 import { generateEventName } from '../helpers/helpers';
 
 
-// position of handler in HandlerItem
-const HANDLER_ID_POSITION = 0;
-const HANDLER_POSITION = 1;
+// // position of handler in HandlerItem
+// const HANDLER_ID_POSITION = 0;
+// const HANDLER_POSITION = 1;
 export const SUBSCRIBE_TOPIC = 'subscribeToRemoteEvent';
 export const UNSUBSCRIBE_TOPIC = 'unsubscribeFromRemoteEvent';
 export const SUBSCRIBE_CATEGORY = 'subscribeToRemoteCategoryEvent';
 export const UNSUBSCRIBE_CATEGORY = 'unsubscribeFromRemoteCategoryEvent';
 export const RESPOND_TOPIC = 'respondOfRemoteEvent';
+// category where will handlers which react on remote evens will be places
+export const SUBSCRIBER_SPECIAL_CATEGORY = 'subscrSpecCat';
 
 type Handler = (message: Message) => void;
-type HandlerItem = [ string, Handler ];
+//type HandlerItem = [ string, Handler ];
 
 
 /**
@@ -29,7 +30,7 @@ export default class BridgeSubscriber {
   private readonly system: System;
   private readonly messenger: Messenger;
   // handlers of remote events by "toHost-category-topic"
-  private readonly handlers: {[index: string]: Array<HandlerItem>} = {};
+  //private readonly handlers: {[index: string]: Array<HandlerItem>} = {};
 
   constructor(system: System, messenger: Messenger) {
     this.system = system;
@@ -43,46 +44,87 @@ export default class BridgeSubscriber {
   /**
    * Subscribe to remote host's events.
    * It sends special message to remote host, remote host responds messages on each event.
-   * And it listens to this messages of remote evens.
+   * And it listens to this messages of remote events.
    */
-  subscribe(toHost: string, category: string, topic: string, handler: Handler): void {
+  subscribe(toHost: string, category: string, topic: string, handler: Handler): string {
+    if (this.messenger.isLocalHost(toHost)) {
+      // subscribe to local events
+      const handlerIndex: number = this.system.events.addListener(category, topic, handler);
+      // convert local index number to string
+      return handlerIndex.toString();
+    }
+
+    // else make remote handler
+    
     const handlerId: string = this.system.host.generateUniqId();
     const message: Message = this.generateSpecialMessage(handlerId, SUBSCRIBE_TOPIC, toHost, category, topic);
 
     // listen to messages from remote host
-    this.addHandler(toHost, category, topic, handlerId, handler);
+    //this.addHandler(toHost, category, topic, handlerId, handler);
+    this.system.events.addListener(SUBSCRIBER_SPECIAL_CATEGORY, handlerId, handler);
 
     // add handler of events of remote host
     this.system.network.send(toHost, message)
       .catch((err) => {
         // TODO: ожидать ответа - если не дошло - наверное повторить
       });
+
+    return handlerId;
   }
 
-  subscribeCategory(toHost: string, category: string, handler: Handler) {
+  subscribeCategory(toHost: string, category: string, handler: Handler): string {
+    if (this.messenger.isLocalHost(toHost)) {
+      // subscribe to local events
+      const handlerIndex: number = this.system.events.addCategoryListener(category, handler);
+      
+      // convert local index number to string
+      return handlerIndex.toString();
+    }
+
+    // else make remote handler
+
     const handlerId: string = this.system.host.generateUniqId();
     const message: Message = this.generateSpecialMessage(handlerId, SUBSCRIBE_CATEGORY, toHost, category);
 
     // listen to messages from remote host
-    this.addHandler(toHost, category, undefined, handlerId, handler);
+    //this.addHandler(toHost, category, undefined, handlerId, handler);
+
+    this.system.events.addListener(SUBSCRIBER_SPECIAL_CATEGORY, handlerId, handler);
 
     // add handler of events of remote host
     this.system.network.send(toHost, message)
       .catch((err) => {
         // TODO: ожидать ответа - если не дошло - наверное повторить
       });
+
+    return handlerId;
   }
 
-  unsubscribe(toHost: string, category: string, topic: string, handler: Handler): void {
-    const eventName = generateEventName(category, topic, toHost);
-    const handlerId = this.findHandlerIdByHandler(eventName, handler);
+  unsubscribe(toHost: string, category: string, topic: string, handlerId: string): void {
+    if (this.messenger.isLocalHost(toHost)) {
+      // subscribe to local events
+      const handlerIndex = parseInt(handlerId);
 
-    if (!handlerId) return;
+      if (Number.isNaN(handlerIndex)) {
+        this.system.log.error(
+          `BridgeSubscriber.subscribeCategory("${toHost}", "${category}", "${handlerId}"). ` +
+          `Wrong handlerId: "${handlerId}". Local handlerId has to be a number as a string.`
+        );
+      }
+
+      return this.system.events.removeListener(category, topic, handlerIndex);
+    }
+
+    //const eventName = generateEventName(category, topic, toHost);
+    //const handlerId = this.findHandlerIdByHandler(eventName, handler);
+
+    //if (!handlerId) return;
 
     const message: Message = this.generateSpecialMessage(handlerId, UNSUBSCRIBE_TOPIC, toHost, category, topic);
 
     // remove local handler
-    this.removeHandler(eventName, handler);
+    //this.removeHandler(eventName, handlerId);
+    this.system.events.removeAllListeners(SUBSCRIBER_SPECIAL_CATEGORY, handlerId);
 
     // send message to remove remote emitter
     this.system.network.send(toHost, message)
@@ -91,80 +133,37 @@ export default class BridgeSubscriber {
       });
   }
 
-  unsubscribeCategory(toHost: string, category: string, handler: Handler) {
-    const eventName = generateEventName(category, undefined, toHost);
-    const handlerId = this.findHandlerIdByHandler(eventName, handler);
+  unsubscribeCategory(toHost: string, category: string, handlerId: string) {
+    if (this.messenger.isLocalHost(toHost)) {
+      // subscribe to local events
+      const handlerIndex = parseInt(handlerId);
 
-    if (!handlerId) return;
+      if (Number.isNaN(handlerIndex)) {
+        this.system.log.error(
+          `BridgeSubscriber.subscribeCategory("${toHost}", "${category}", "${handlerId}"). ` +
+          `Wrong handlerId. Local handlerId has to be a number as a string.`
+        );
+      }
+
+      return this.system.events.removeCategoryListener(category, handlerIndex);
+    }
+
+    const eventName = generateEventName(category, undefined, toHost);
+    //const handlerId = this.findHandlerIdByHandler(eventName, handler);
+
+    //if (!handlerId) return;
 
     const message: Message = this.generateSpecialMessage(handlerId, UNSUBSCRIBE_CATEGORY, toHost, category);
 
     // remove local handler
-    this.removeHandler(eventName, handler);
+    //this.removeHandler(eventName, handlerId);
+    this.system.events.removeAllListeners(SUBSCRIBER_SPECIAL_CATEGORY, handlerId);
 
     // send message to remove remote emitter
     this.system.network.send(toHost, message)
       .catch((err) => {
         // TODO: ожидать ответа - если не дошло - наверное повторить
       });
-  }
-
-  /**
-   * Register handler which will be risen on remote event
-   */
-  private addHandler(
-    toHost: string,
-    category: string,
-    topic: string | undefined,
-    handlerId: string,
-    handler: Handler
-  ) {
-    const eventName = generateEventName(category, topic, toHost);
-    const handlerItem: HandlerItem = [ handlerId, handler ];
-
-    if (!this.handlers[eventName]) this.handlers[eventName] = [];
-
-    // register listener
-    this.handlers[eventName].push(handlerItem);
-  }
-
-  private removeHandler(eventName: string, handler: Function): void {
-    const handlers: Array<HandlerItem> = this.handlers[eventName];
-    const handlerIndex: number = handlers.findIndex((item: HandlerItem) => {
-      return item[HANDLER_POSITION] === handler;
-    });
-
-    if (handlerIndex < 0) throw new Error(`Can't find handler index of "${eventName}"`);
-
-    // remove handler item
-    handlers.splice(handlerIndex, 1);
-    // remove container
-    if (!this.handlers[eventName].length) delete this.handlers[eventName];
-  }
-
-  private findHandlerIdByHandler(eventName: string, handler: Function): string {
-    const handlers = this.handlers[eventName];
-    const handlerItem: HandlerItem | undefined = _find(handlers, (item: HandlerItem) => {
-      return item[HANDLER_POSITION] === handler;
-    });
-
-    if (!handlerItem) throw new Error(`Can't find handler of "${eventName}"`);
-
-    return handlerItem[HANDLER_ID_POSITION];
-  }
-
-  private findHandlerById(eventName: string, handlerId: string): Function {
-    const handlers = this.handlers[eventName];
-
-    if (!handlers) throw new Error(`Can't find handlers of "${eventName}"`);
-
-    const handlerItem: HandlerItem | undefined = _find(handlers, (item: HandlerItem) => {
-      return item[HANDLER_ID_POSITION] === handlerId;
-    });
-
-    if (!handlerItem) throw new Error(`Can't find handlerId of "${eventName}" and handler id ${handlerId}`);
-
-    return handlerItem[HANDLER_POSITION];
   }
 
   /**
@@ -190,15 +189,16 @@ export default class BridgeSubscriber {
     if (!this.checkIncomeMsgPayload(payload)) return;
 
     // call subscriber with remote data
-    const eventName = generateEventName(payload.category, payload.topic, remoteHost);
-    const handler = this.findHandlerById(eventName, payload.hadlerId);
+    //const eventName = generateEventName(payload.category, payload.topic, remoteHost);
+    //const handler = this.findHandlerById(eventName, payload.handlerId);
 
     // TODO: если пришло сообщение на которое нет подписки - вызвать unsubscribe и писать в лог
 
     // TODO: на самом деле handler ожидает сообщение, а тут не совсем сообщение {category, topic, handlerId, payload}
 
     // call handler
-    handler(payload);
+    //handler(payload);
+    this.system.events.emit(SUBSCRIBER_SPECIAL_CATEGORY, payload.handlerId, payload);
   }
 
   /**
@@ -230,5 +230,67 @@ export default class BridgeSubscriber {
 
     return payload.category && payload.topic && payload.handlerId;
   }
+
+
+
+  // private removeHandler(eventName: string, handlerId: string): void {
+  //   //const handlers: Array<HandlerItem> = this.handlers[eventName];
+  //   // const handlerIndex: number = handlers.findIndex((item: HandlerItem) => {
+  //   //   return item[HANDLER_POSITION] === handler;
+  //   // });
+  //
+  //   //if (handlerIndex < 0) throw new Error(`Can't find handler index of "${eventName}"`);
+  //
+  //   // remove handler item
+  //   //handlers.splice(handlerIndex, 1);
+  //   // remove container
+  //   //if (!this.handlers[eventName].length) delete this.handlers[eventName];
+  //
+  //
+  // }
+
+  // /**
+  //  * Register handler which will be risen on remote event
+  //  */
+  // private addHandler(
+  //   toHost: string,
+  //   category: string,
+  //   topic: string | undefined,
+  //   handlerId: string,
+  //   handler: Handler
+  // ) {
+  //   const eventName = generateEventName(category, topic, toHost);
+  //   const handlerItem: HandlerItem = [ handlerId, handler ];
+  //
+  //   if (!this.handlers[eventName]) this.handlers[eventName] = [];
+  //
+  //   // register listener
+  //   this.handlers[eventName].push(handlerItem);
+  // }
+
+  // private findHandlerIdByHandler(eventName: string, handler: Function): string {
+  //   const handlers = this.handlers[eventName];
+  //   const handlerItem: HandlerItem | undefined = _find(handlers, (item: HandlerItem) => {
+  //     return item[HANDLER_POSITION] === handler;
+  //   });
+  //
+  //   if (!handlerItem) throw new Error(`Can't find handler of "${eventName}"`);
+  //
+  //   return handlerItem[HANDLER_ID_POSITION];
+  // }
+
+  // private findHandlerById(eventName: string, handlerId: string): Function {
+  //   const handlers = this.handlers[eventName];
+  //
+  //   if (!handlers) throw new Error(`Can't find handlers of "${eventName}"`);
+  //
+  //   const handlerItem: HandlerItem | undefined = _find(handlers, (item: HandlerItem) => {
+  //     return item[HANDLER_ID_POSITION] === handlerId;
+  //   });
+  //
+  //   if (!handlerItem) throw new Error(`Can't find handlerId of "${eventName}" and handler id ${handlerId}`);
+  //
+  //   return handlerItem[HANDLER_POSITION];
+  // }
 
 }
