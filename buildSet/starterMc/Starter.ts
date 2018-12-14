ae8b4e45fd459ec3df954e953a84c06b531923d9
Ivan <ipkozyrin@gmail.com>
2018-12-14 10:27:08 +1000

    add

--- buildSet/starterMc/Starter.ts
+++ buildSet/starterMc/Starter.ts
@@ -1,7 +1,13 @@
 import * as fs from 'fs';
 
 import Main from './Main';
-import {eachFileRecursivelly} from './helpers';
+import {eachFileRecursivelly, makeModuleName} from './helpers';
+
+
+declare const Modules: {
+  addCached: (moduleName: string, content: string) => void;
+  removeAllCached: () => void;
+};
 
 
 export default class Starter {
@@ -16,8 +22,16 @@ export default class Starter {
   async start() {
     const hostDir = `${this.main.config.rootDir}/${this.main.config.hostDir}`;
 
-    eachFileRecursivelly(hostDir, (pathToFile: string) => {
+    // remove all the modules
+    Modules.removeAllCached();
 
+    eachFileRecursivelly(hostDir, (pathToFile: string) => {
+      const fileContent: string = fs.readFileSync(pathToFile) as any;
+      const moduleName: string = makeModuleName(pathToFile, hostDir, 'host');
+
+      console.log(1111111, pathToFile);
+
+      Modules.addCached(moduleName, fileContent);
     });
 
     console.log(33333333, fs.readdirSync('/'));
--- buildSet/starterMc/helpers.ts
+++ buildSet/starterMc/helpers.ts
@@ -1,3 +1,8 @@
 export function eachFileRecursivelly(rootDir: string, cb: (pathToFile: string) => void) {
+  // TODO: add
+  // TODO: проверить что папка существует и есть файлы
+}
 
+export function makeModuleName(pathToFile: string, removeRoot: string, newRoot: string): string {
+  // TODO: add
 }
