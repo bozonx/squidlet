8c605c85f3c36979e0a144e4272ade1012c469b9
Ivan <ipkozyrin@gmail.com>
2018-12-20 13:07:05 +1000

    refactor

--- buildSet/buildHelpers/prepareToFlash.js
+++ buildSet/buildHelpers/prepareToFlash.js
@@ -7,8 +7,9 @@ const {makeModulesTree, stripExtension} = require('./helpers');
 
 
 const fsPromises = fs.promises;
-const FULL_FILE_NAME_POS = 0;
-const DST_FILE_NAME_POS = 1;
+const HASH_POS = 0;
+const FULL_FILE_NAME_POS = 1;
+const DST_FILE_NAME_POS = 2;
 
 
 class PrepareToFlash {
@@ -17,12 +18,13 @@ class PrepareToFlash {
     this.dstDir = dstDir;
     this.relativeIndexFile = relativeIndexFile;
     this.moduleRoot = moduleRoot;
-    this.modules = {};
+    this.localModules = [];
   }
 
   async start() {
     const modulesFileNames = makeModulesTree(this.srcDir, this.relativeIndexFile);
-    this.modules = this.makeModulesNames(modulesFileNames);
+
+    this.localModules = this.makeLocalModulesNames(modulesFileNames);
 
     try {
       await fsPromises.mkdir(this.dstDir);
@@ -31,16 +33,13 @@ class PrepareToFlash {
       // do nothing
     }
 
-    await this.copyToFlashDir();
+    await this.copyLocalToFlashDir();
     await this.renameLocalModulesRequires();
   }
 
 
-  makeModulesNames(modulesFileNames) {
-
-    // TODO: поидее можно просто список сделать
-
-    const result = {};
+  makeLocalModulesNames(modulesFileNames) {
+    const result = [];
 
     for (let fullFileName of modulesFileNames) {
       const relativeFilePath = path.relative(this.srcDir, fullFileName);
@@ -49,45 +48,39 @@ class PrepareToFlash {
       const moduleNameHash = hashSum(moduleName);
       const dstFileName = path.join(this.dstDir, moduleNameHash);
 
-      result[moduleNameHash] = [
+      result.push([
+        moduleNameHash,
         fullFileName,
         dstFileName,
-      ];
+      ]);
     }
 
     return result;
   }
 
-  async copyToFlashDir() {
-    for (let moduleNameHash of Object.keys(this.modules)) {
+  async copyLocalToFlashDir() {
+    for (let localModule of this.localModules) {
       await fsPromises.copyFile(
-        this.modules[moduleNameHash][FULL_FILE_NAME_POS],
-        this.modules[moduleNameHash][DST_FILE_NAME_POS]
+        localModule[FULL_FILE_NAME_POS],
+        localModule[DST_FILE_NAME_POS]
       );
     }
   }
 
-  getHashOrRelativeModule(srcFileFullPath, requiredModuleRelPath) {
+  getHashOfRelativeModule(srcFileFullPath, requiredModuleRelPath) {
     const baseDir = path.dirname(srcFileFullPath);
     const depFullPath = path.resolve(baseDir, `${requiredModuleRelPath}.js`);
-    let depHashName;
-    const foundItem = _.find(this.modules, (item, index) => {
-      if (item[FULL_FILE_NAME_POS] === depFullPath) {
-        depHashName = index;
+    const foundItem = _.find(this.localModules, (item) => item[FULL_FILE_NAME_POS] === depFullPath);
 
-        return true;
-      }
-    });
-
-    if (!depHashName) {
+    if (!foundItem) {
       throw new Error(`Can't find local module "${requiredModuleRelPath}" in "${foundItem[FULL_FILE_NAME_POS]}"`);
     }
 
-    return depHashName;
+    return foundItem[HASH_POS];
   }
 
   getHashOfDependency() {
-
+    // TODO:
   }
 
   replaceRequirePaths(moduleContent, srcFileFullPath) {
@@ -101,7 +94,7 @@ class PrepareToFlash {
       // skip not local modules
       if (savedPart.match(/^\./)) {
         // local module
-        depHashName = this.getHashOrRelativeModule(srcFileFullPath, savedPart);
+        depHashName = this.getHashOfRelativeModule(srcFileFullPath, savedPart);
       }
       else {
         // third party dependency
@@ -113,18 +106,18 @@ class PrepareToFlash {
   }
 
   async renameLocalModulesRequires() {
-    for (let moduleNameHash of Object.keys(this.modules)) {
+    for (let localModule of this.localModules) {
       const moduleContent = await fsPromises.readFile(
-        this.modules[moduleNameHash][DST_FILE_NAME_POS],
+        localModule[DST_FILE_NAME_POS],
         {encoding: 'utf8'}
       );
       const replacedContent = this.replaceRequirePaths(
         moduleContent,
-        this.modules[moduleNameHash][FULL_FILE_NAME_POS]
+        localModule[FULL_FILE_NAME_POS]
       );
 
       await fsPromises.writeFile(
-        this.modules[moduleNameHash][DST_FILE_NAME_POS],
+        localModule[DST_FILE_NAME_POS],
         replacedContent,
         {encoding: 'utf8'}
       );
