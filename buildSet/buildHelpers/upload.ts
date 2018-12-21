33136954ec85d434be0f04c817a12176018e57ec
Ivan <ipkozyrin@gmail.com>
2018-12-21 12:21:36 +1000

    refactor

--- buildSet/buildHelpers/upload.ts
+++ buildSet/buildHelpers/upload.ts
@@ -1,3 +1,5 @@
+declare const Espruino: {[index: string]: any};
+
 import * as fs from 'fs';
 import * as path from 'path';
 const esp = require('espruino');
@@ -8,81 +10,105 @@ const {stringify} = require('./helpers');
 const fsPromises = fs.promises;
 
 
-function runExprs (port, exprs) {
-  Espruino.Core.Serial.startListening(function(data) { });
+class Upload {
+  private readonly board: string;
+  private readonly port: string;
+  private readonly portSpeed: number;
+  private readonly flashDir: string;
 
-  const sendExpression = (exp) => {
+
+  constructor(board: string, port: string, portSpeed: number, flashDir: string) {
+    this.board = board;
+    this.port = port;
+    this.portSpeed = portSpeed;
+    this.flashDir = flashDir;
+  }
+
+  async start() {
+    await this.initEspruino();
+    this.configureEspruino();
+
+    const uploadExpressions = await this.collectUploadExpressions();
+
+    await this.runExprs(uploadExpressions);
+  }
+
+  private async collectUploadExpressions(): Promise<string[]> {
+    const filesInFlashDir: string[] = await fsPromises.readdir(this.flashDir);
+    const exprs: string[] = [];
+
+    for (let relFileName of filesInFlashDir) {
+      const fullFileName = path.resolve(this.flashDir, relFileName);
+      const moduleContent: string = await fsPromises.readFile(fullFileName, {encoding: 'utf8'});
+      const stringedModule = stringify(moduleContent);
+      const expr = `require("Storage").write("${relFileName}", "${stringedModule}")`;
+
+      exprs.push(expr);
+    }
+
+    return exprs;
+  }
+
+  private runExprs (uploadExpressions: string[]) {
+    Espruino.Core.Serial.startListening(() => {});
+
     return new Promise((resolve, reject) => {
-      Espruino.Core.Utils.executeExpression(exp, function(result) {
+      Espruino.Core.Serial.open(this.port, async (status?: string) => {
+          if (status === undefined) {
+            return reject(new Error('Unable to connect!'));
+          }
+
+          for (let exp of uploadExpressions) {
+            await this.sendExpression(exp);
+          }
+
+          Espruino.Core.Serial.close();
+        },
+        () => { // disconnected
+          //if (callback) callback(exprResult);
+          resolve();
+        });
+    });
+  }
+
+  sendExpression(expression: string) {
+    return new Promise((resolve, reject) => {
+      Espruino.Core.Utils.executeExpression(expression, (result?: string) => {
         if (result) {
-          setTimeout(function() {
+          setTimeout(() => {
             return resolve(result);
           }, 500);
 
           return;
         }
 
-        reject(`No result of exp: ${exp}`);
+        reject(`No result of expression: ${expression}`);
       });
     });
-  };
+  }
 
-  return new Promise((resolve, reject) => {
-    Espruino.Core.Serial.open(port, async function(status) {
-      if (status === undefined) {
-        return reject(new Error('Unable to connect!'));
-      }
+  private configureEspruino() {
+    Espruino.Config.BAUD_RATE = String(this.portSpeed);
+    Espruino.Config.BLUETOOTH_LOW_ENERGY  = false;
+    Espruino.Config.SET_TIME_ON_WRITE  = true;
+    Espruino.Config.BOARD_JSON_URL = `http://www.espruino.com/json/${this.board}.json`;
+  }
 
-      for (let exp of exprs) {
-        await sendExpression(exp);
-      }
+  private initEspruino() {
+    return new Promise((resolve, reject) => {
+      esp.init((err?: string) => {
+        if (err) return reject(new Error(err));
 
-      Espruino.Core.Serial.close();
-    },
-    function() { // disconnected
-      //if (callback) callback(exprResult);
-      resolve();
+        resolve();
+      });
     });
-  });
-}
+  }
 
-function configureEspruino(board, portSpeed) {
-  Espruino.Config.BAUD_RATE = String(portSpeed);
-  Espruino.Config.BLUETOOTH_LOW_ENERGY  = false;
-  Espruino.Config.SET_TIME_ON_WRITE  = true;
-  Espruino.Config.BOARD_JSON_URL = `http://www.espruino.com/json/${board}.json`;
-}
-
-function initEspruino() {
-  return new Promise((resolve, reject) => {
-    esp.init((err) => {
-      if (err) return reject(err);
-
-      resolve();
-    });
-  });
 }
 
 
 export default async function (board: string, port: string, portSpeed: number, flashDir: string) {
-  await initEspruino();
-  configureEspruino(board, portSpeed);
+  const upload = new Upload(board, port, portSpeed, flashDir);
 
-  const files = await fsPromises.readdir(flashDir);
-  const exprs = [];
-
-  for (let relFileName of files) {
-    const fullFileName = path.resolve(flashDir, relFileName);
-    const moduleContent = await fsPromises.readFile(fullFileName, {encoding: 'utf8'});
-    const stringedModule = stringify(moduleContent);
-    const expr = `require("Storage").write("${relFileName}", "${stringedModule}")`;
-
-    //console.log(`-----> Writing file ${relFileName}`);
-
-    //await runExp(port, expr);
-
-    exprs.push(expr);
-  }
-
-  await runExprs(port, exprs);
+  await upload.start();
 }
