3e6c5cb943ac022a75d94342e07e1ce7c8658c63
Ivan <ipkozyrin@gmail.com>
2019-01-27 11:10:12 +1000

    add

---
+++ squidlet-nodejs/esp32-x86.ts
@@ -0,0 +1,36 @@
+import PlatformConfig from '../squidlet-starter/buildHostEnv/interfaces/PlatformConfig';
+
+
+const platformConfig: PlatformConfig = {
+  devs: [
+    'Storage.dev',
+    'Sys.dev',
+    //'Sha1',
+    'Digital.dev',
+    //'Pwm.dev',
+    'Serial.dev',
+    //'Spi.dev',
+    //'Wifi.dev',
+    //'Bluetooth.dev',
+    'I2cMaster.dev',
+
+    'Mqtt.dev',
+
+    //'Adc.dev',
+    //'Dac.dev',
+    //'I2cSlave.dev',
+  ],
+
+  hostConfig: {
+    config: {
+    },
+    drivers: {
+      'I2cMaster.driver': {
+        bus: 1,
+      }
+    },
+
+  }
+};
+
+export default platformConfig;
