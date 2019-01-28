ea5b413670d1d4dbc9d8cfad9799dc9dc07cdad2
Ivan <ipkozyrin@gmail.com>
2019-01-28 14:00:50 +1000

    add

--- squidlet-lowjs/tasks.ts
+++ squidlet-lowjs/tasks.ts
@@ -14,11 +14,7 @@ import BuildConfig from '../squidlet-starter/buildHostEnv/interfaces/BuildConfig
 import PreHostConfig from '../squidlet-starter/buildHostEnv/interfaces/PreHostConfig';
 
 
-//const machineName: string = yargs.argv.machine as string;
 const envConfigRelPath: string = yargs.argv.config as string;
-
-
-// TODO: нужно ещё как-то передать конфиг в configWorks
 
 
 gulp.task('build-devs', async () => {
@@ -38,26 +34,27 @@ gulp.task('build', async () => {
   const buildConfig: BuildConfig = _.defaultsDeep({}, envConfig.buildConfig, makeBuildConfig(__dirname));
 
   if (!envConfig.machine) {
-    throw new Error(`You have to specify a machine param in your host config`);
+    throw new Error(`You have to specify a "machine" param in your host config`);
+  }
+  else if (!envConfig.id) {
+    throw new Error(`You have to specify an "id" param in your host config`);
   }
 
   const machineConfigFilePath: string = path.resolve(__dirname, `./${envConfig.machine}.ts`);
   const machineConfig: PlatformConfig = require(machineConfigFilePath).default;
   const hostBuildDir: string = path.join(buildConfig.buildDir, envConfig.id);
+  const devsDstDir: string = path.join(hostBuildDir, 'devs');
 
-  shelljs.mkdir('-p', buildConfig.devsLegacyDst);
-
-
-  console.log(111111111, machineConfig)
+  shelljs.mkdir('-p', devsDstDir);
 
   // copy specified devs
   for (let devName of machineConfig.devs) {
-    const devSrcFile: string = path.join(buildConfig.devsLegacyDst, `${devName}.ts`);
-    const devDstFile: string = path.join(hostBuildDir, `devs/${devName}.ts`);
+    const devSrcFile: string = path.join(buildConfig.devsLegacyDst, `${devName}.js`);
 
-    console.log(22222222, devSrcFile, devDstFile)
-
-
-    shelljs.cp('-f', devSrcFile, devDstFile);
+    shelljs.cp('-f', devSrcFile, devsDstDir);
   }
+
+  // TODO: copy host
+  // TODO: нужно ещё как-то передать конфиг в configWorks
+
 });
