94796b5f6335dfc22c45f77375d6f54c1e928e73
Ivan <ipkozyrin@gmail.com>
2019-01-28 11:52:48 +1000

    refactor

--- squidlet-lowjs/tasks/tasks.ts
+++ squidlet-lowjs/tasks/tasks.ts
@@ -5,7 +5,13 @@ import * as yargs from 'yargs';
 import PlatformConfig from '../../squidlet-starter/buildHostEnv/interfaces/PlatformConfig';
 
 
+const BUILD_DIR = path.resolve(__dirname, `./build`);
+
+
 gulp.task('build-devs', async () => {
+  if (!yargs.argv.machine) {
+    throw new Error(`You have to specify a machine name`);
+  }
 
   //console.log(yargs.argv)
 
@@ -14,6 +20,12 @@ gulp.task('build-devs', async () => {
 
   // TODO: нужно ещё как-то передать конфиг в configWorks
 
-  const machineConfigFilePath: string = `./${yargs.argv.machine}.ts`;
+  const machineConfigFilePath: string = path.resolve(__dirname, `./${yargs.argv.machine}.ts`);
   const machineConfig: PlatformConfig = require(machineConfigFilePath);
+  const machineBuildDir: string = path.resolve(BUILD_DIR, yargs.argv.machine);
+
+  for (let devName of machineConfig.devs) {
+    const devFilePath: string = path.resolve(__dirname, `./${devName}.ts`);
+
+  }
 });
