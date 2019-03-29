edf484f5bbf114a65da589ca2b20db00e48195f3
Ivan <ipkozyrin@gmail.com>
2019-03-29 10:29:07 +1000

    add

--- hostEnvBuilder/hostConfig/validateDefinitions.ts
+++ hostEnvBuilder/hostConfig/validateDefinitions.ts
@@ -1,29 +1,6 @@
 import SchemaElement from '../../host/interfaces/SchemaElement';
-
-
-// function checkDevices(rawConfig: {[index: string]: any}): string | undefined {
-//   // TODO: add
-// }
-//
-// function checkDrivers(rawConfig: {[index: string]: any}): string | undefined {
-//   // TODO: проверить что есть такой драйвер
-// }
-//
-// function checkServices(rawConfig: {[index: string]: any}): string | undefined {
-//   // TODO: add
-// }
-//
-// function checkAutomation(rawConfig: {[index: string]: any}): string | undefined {
-//   // TODO: add
-// }
-//
-// function checkMqtt(rawConfig: {[index: string]: any}): string | undefined {
-//   // TODO: add
-// }
-//
-// function checkLogger(rawConfig: {[index: string]: any}): string | undefined {
-//   // TODO: add
-// }
+import {whiteList} from './validationHelpers';
+import {isValueOfType} from '../../host/helpers/typesHelpers';
 
 
 export default function validateProps(
@@ -32,41 +9,15 @@ export default function validateProps(
 ): string | undefined {
   if (!schema) return;
 
+  const whiteListErr: string | undefined = whiteList(props, Object.keys(schema), 'props');
 
+  if (whiteListErr) return whiteListErr;
+
+  for (let name of Object.keys(props)) {
+    const typeErr: string | undefined = isValueOfType(schema[name].type, props[name]);
+
+    if (typeErr) return `prop "${name}": typeErr`;
+  }
 
   return;
 }
-
-
-// TODO: validate definitions
-
-// TODO: main files and files of entities должны быть относительные пути
-// TODO: id драйвера должно совпадать с полем driver у definition
-
-// const recursive = (container, curPath) => {
-//   _.each(container, (item, name) => {
-//     const itemPath = _.trimStart(`${curPath}.${name}`, '.');
-//
-//     if (_.isString(item)) {
-//       // TODO: validate type
-//     }
-//     else if (_.isPlainObject(item) && item.type) {
-//       // TODO: validate type
-//     }
-//     else if (_.isPlainObject(item)) {
-//       recursive(item, itemPath);
-//     }
-//     else {
-//       throw new Error(`Can't parse schema of device ${deviceName}`);
-//     }
-//   });
-//
-// };
-//
-// recursive(schema.params, '');
-
-// TODO: проверить существование файлов из манифеста
-
-// if (!await this.fs.exists(entitySet.main)) {
-//   throw new Error(`Can't find main file "${entitySet.main}"`);
-// }
