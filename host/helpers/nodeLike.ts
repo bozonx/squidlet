255d9bf363ca6b73b9b7813e9918aa9567eccbb4
Ivan <ipkozyrin@gmail.com>
2019-04-03 09:54:12 +1000

    test

--- host/helpers/nodeLike.ts
+++ host/helpers/nodeLike.ts
@@ -3,16 +3,31 @@ import {PATH_SEPARATOR} from './helpers';
 const SEP = '/';
 
 
+/**
+ * Join paths.
+ * pathJoin('/path/', '/to/', './dir') => '/path/to/./dir'
+ */
 export function pathJoin (...paths: string[]): string {
   const prepared: string[] = [];
 
   for (let i = 0; i < paths.length; i++) {
-    if (i === 0) {
-      prepared.push(paths[i]);
+    let cleared: string;
+
+    if (typeof paths[i] !== 'undefined') {
+      throw new Error(`pathJoin: paths has to be strings`);
+    }
+    else if (i === 0) {
+      const regexp = `\\${SEP}$`;
+
+      cleared = paths[i].replace(new RegExp(regexp), '');
     }
     else {
-      prepared.push(paths[i].replace(/^\//, ''));
+      const regexp = `^\\${SEP}(.+)\\${SEP}$`;
+
+      cleared = paths[i].replace(new RegExp(regexp), '$1');
     }
+
+    prepared.push(cleared);
   }
 
   return prepared.join(SEP);
--- test/host/helpers/nodeLike.spec.coffee
+++ test/host/helpers/nodeLike.spec.coffee
@@ -2,5 +2,8 @@ nodeLike = require('../../../host/helpers/nodeLike');
 
 
 describe.only 'helpers.nodeLike', ->
-  it "...", ->
+  it "pathJoin", ->
+    assert.equal(nodeLike.pathJoin('/path/', '/to/', './dir'), '/path/to/./dir')
 
+  it "pathJoin", ->
+    assert.equal(nodeLike.pathJoin('/path/', '/to/', './dir'), '/path/to/./dir')
