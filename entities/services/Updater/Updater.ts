d6ebb9a8b22f1800bb195661854b3b41796aae57
Ivan <ipkozyrin@gmail.com>
2019-04-10 08:56:15 +1000

    refactor

--- entities/services/Updater/Updater.ts
+++ entities/services/Updater/Updater.ts
@@ -1,5 +1,4 @@
 import ServiceBase from 'host/baseServices/ServiceBase';
-import LogLevel from 'host/interfaces/LogLevel';
 import categories from 'host/dict/categories';
 import topics from 'host/dict/topics';
 
@@ -7,7 +6,7 @@ import topics from 'host/dict/topics';
 interface Props {
 }
 
-export default class Udater extends ServiceBase<Props> {
+export default class Updater extends ServiceBase<Props> {
   protected didInit = async () => {
     this.listen();
   }
@@ -22,16 +21,6 @@ export default class Udater extends ServiceBase<Props> {
     this.env.events.addListener(categories.updater, topics.updater.updateHost, this.onUpdateHost);
     this.env.events.addListener(categories.updater, topics.updater.updateFile, this.onUpdateFile);
     this.env.events.addListener(categories.updater, topics.updater.removeFile, this.onRemove);
-
-    // const hosts: string[] = ['master'];
-    //
-    // for (let hostId of hosts) {
-    //
-    //   // listen to allowed levels
-    //   for (let level of allowedLogLevels) {
-    //     this.env.events.addListener(categories.logger, level, this.logEventsHandler);
-    //   }
-    // }
   }
 
   private onUpdateEntity = (data: null) => {
