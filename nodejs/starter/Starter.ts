58b9a7648502d6c2d471a358e96f6eff210c9c95
Ivan <ipkozyrin@gmail.com>
2019-04-12 13:17:19 +1000

    add

--- nodejs/starter/Starter.ts
+++ nodejs/starter/Starter.ts
@@ -1,8 +1,11 @@
+import * as path from 'path';
+
 import Io from '../../hostEnvBuilder/Io';
 import ResolveArgs from './ResolveArgs';
 import GroupConfigParser from '../../control/GroupConfigParser';
 import Props from './Props';
 import DevsSet from './DevsSet';
+import systemConfig from '../../host/config/systemConfig';
 
 
 export default class Starter {
@@ -37,17 +40,23 @@ export default class Starter {
     // TODO: если нету system - то билдится он и env set с конфигом по умолчанию
   }
 
-  async startProdSystem() {
-
-    // TODO: запускается System из workDir с этим devset
-  }
-
   async buildDevelopEnvSet() {
     // TODO: сбилдить конфиги хоста где указанны пути к реальному главному ts файлу
   }
 
+  async startProdSystem() {
+    const pathToSystem = path.join(this.props.workDir, systemConfig.rootDirs.host, 'System');
+    const System = require(pathToSystem).default;
+    const system = new System(this.devSet.devSet);
+
+    return system.start();
+  }
+
   async startDevelopSystem() {
-    // TODO:
+    const System = require(`../../host/System`).default;
+    const system = new System(this.devSet.devSet);
+
+    return system.start();
   }
 
 }
