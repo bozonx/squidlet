7f173d91e56b96827dd96b67e6c71d7bcd09d696
Ivan <ipkozyrin@gmail.com>
2019-05-01 12:53:40 +1000

    refactor - return pigpio Digital

--- nodejs/devs/Digital.ts
+++ nodejs/devs/Digital.ts
@@ -1,23 +1,5 @@
-/*
- * It uses a pigpiod daemon.
- *
- * Control pigpiod:
- *
- * Run in foreground
- *     sudo pigpiod -g
- *
- * start a daemon
- *     sudo pigpiod
- *
- * stop a daemon
- *     sudo killall pigpiod
- */
-
 // @ts-ignore
-const pigpio = require('pigpio-client').pigpio({
-  //host: '0.0.0.0'
-  //timeout: 1,
-});
+import {Gpio} from 'pigpio';
 
 import DigitalIo, {
   Edge,
@@ -26,63 +8,22 @@ import DigitalIo, {
   DigitalInputMode
 } from 'system/interfaces/io/DigitalIo';
 import DebounceCall from 'system/helpers/DebounceCall';
-import {callPromised} from 'system/helpers/helpers';
 
 
-type GpioHandler = (level: number, tick: number) => void;
+type GpioHandler = (level: number) => void;
 
 interface Listener {
   pin: number;
   handler: GpioHandler;
 }
 
-const CONNECTION_TIMEOUT = 60000;
-let wasConnected = false;
-const connectionPromise = new Promise((resolve, reject) => {
-  console.log(`... Connecting to pigpiod daemon`);
-
-  pigpio.once('connected', (info: {[index: string]: string}) => {
-    // display information on pigpio and connection status
-    console.log('SUCCESS: has been connected successfully to the pigpio daemon');
-
-    wasConnected = true;
-    resolve();
-  });
-
-  // Errors are emitted unless you provide API with callback.
-  pigpio.on('error', (err: {message: string})=> {
-    console.error('Application received error: ', err.message); // or err.stack
-
-    if (!wasConnected) reject(`Can't connect: ${JSON.stringify(err)}`);
-  });
-
-  pigpio.on('disconnected', (reason: string) => {
-    console.log('App received disconnected event, reason: ', reason);
-    console.log('App reconnecting in 1 sec');
-    setTimeout( pigpio.connect, 1000, {host: 'raspberryHostIP'});
-  });
-
-  setTimeout(() => {
-    if (wasConnected) return;
-    reject(`Can't connect to pigpiod, timeout has been exceeded`);
-  }, CONNECTION_TIMEOUT);
-});
-
-
-// TODO: все выводы в log выводить в системный логгер (возможно через события)
-
 
 export default class Digital implements DigitalIo {
-  private readonly pinInstances: {[index: string]: any} = {};
+  private readonly pinInstances: {[index: string]: Gpio} = {};
   private readonly alertListeners: Listener[] = [];
   private readonly debounceCall: DebounceCall = new DebounceCall();
   // debounce times by pin number
   private debounceTimes: {[index: string]: number | undefined} = {};
-
-
-  async init() {
-    await connectionPromise;
-  }
 
 
   /**
@@ -90,26 +31,14 @@ export default class Digital implements DigitalIo {
    * It doesn't set an initial value on output pin because a driver have to use it.
    */
   async setupInput(pin: number, inputMode: DigitalInputMode, debounce?: number, edge?: Edge): Promise<void> {
+    const convertedMode: {mode: number, pullUpDown: number} = this.convertMode(inputMode);
     // save debounce time
     this.debounceTimes[pin] = debounce;
-
-    // TODO: edge - this.resolveEdge(edge),
-
-    const pinInstance = pigpio.gpio(pin);
-    this.pinInstances[pin] = pinInstance;
-    await callPromised(pinInstance.modeSet, 'input');
-
-    if (inputMode === 'input_pullup') {
-      await callPromised(pinInstance.pullUpDown, 2);
-    }
-    else if (inputMode === 'input_pulldown') {
-      await callPromised(pinInstance.pullUpDown, 1);
-    }
-
-
-    // Returns a numbr - 0 for input
-    console.log('--------- mode', await callPromised(this.pinInstances[pin].modeGet));
-
+    // make a new instance of Gpio
+    this.pinInstances[pin] = new Gpio(pin, {
+      ...convertedMode,
+      edge: this.resolveEdge(edge),
+    });
   }
 
   /**
@@ -117,11 +46,14 @@ export default class Digital implements DigitalIo {
    * It doesn't set an initial value on output pin because a driver have to use it.
    */
   async setupOutput(pin: number, initialValue?: boolean): Promise<void> {
-    this.pinInstances[pin] = pigpio.gpio(pin);
-    await callPromised(this.pinInstances[pin].modeSet, 'output');
+    const convertedMode: {mode: number, pullUpDown: number} = this.convertMode('output');
+
+    this.pinInstances[pin] = new Gpio(pin, {
+      ...convertedMode,
+    });
 
     // set initial value if is set
-    if (typeof initialValue !== 'undefined') await callPromised(this.write, pin, initialValue);
+    if (typeof initialValue !== 'undefined') await this.write(pin, initialValue);
   }
 
   /**
@@ -130,41 +62,36 @@ export default class Digital implements DigitalIo {
    */
   async getPinMode(pin: number): Promise<DigitalPinMode | undefined> {
     const pinInstance = this.getPinInstance('getPinMode', pin);
-    const modeConst: number = await callPromised(pinInstance.modeGet);
+    const modeConst: number = pinInstance.getMode();
 
-    if (modeConst === 0) {
+    if (modeConst === Gpio.INPUT) {
       return 'input';
 
       // TODO: add support of input_pullup and input_pulldown
     }
-    else {
-
-      // TODO: which const in output ????
-
+    else if (modeConst === Gpio.OUTPUT) {
       return 'output';
     }
+
+    return;
   }
 
   async read(pin: number): Promise<boolean> {
     const pinInstance = this.getPinInstance('read', pin);
 
-    // returns 0 or 1
-    const result: number = await callPromised(pinInstance.read);
-
-    return Boolean(result);
+    return Boolean(pinInstance.digitalRead());
   }
 
   async write(pin: number, value: boolean): Promise<void> {
     const pinInstance = this.getPinInstance('write', pin);
     const numValue = (value) ? 1 : 0;
 
-    return callPromised(pinInstance.write, numValue);
+    pinInstance.digitalWrite(numValue);
   }
 
   async setWatch(pin: number, handler: WatchHandler): Promise<number> {
     const pinInstance = this.getPinInstance('setWatch', pin);
-
-    const handlerWrapper: GpioHandler = (level: number, tick: number) => {
+    const handlerWrapper: GpioHandler = (level: number) => {
       const value: boolean = Boolean(level);
 
       // if undefined or 0 - call handler immediately
@@ -183,8 +110,7 @@ export default class Digital implements DigitalIo {
     // register
     this.alertListeners.push({ pin, handler: handlerWrapper });
     // start listen
-    pinInstance.notify(handlerWrapper);
-
+    pinInstance.on('interrupt', handlerWrapper);
     // return an index
     return this.alertListeners.length - 1;
   }
@@ -200,7 +126,7 @@ export default class Digital implements DigitalIo {
     const {pin, handler} = this.alertListeners[id];
     const pinInstance = this.getPinInstance('clearWatch', pin);
 
-    pinInstance.endNotify(handler);
+    pinInstance.off('interrupt', handler);
 
     delete this.alertListeners[id];
   }
@@ -212,18 +138,45 @@ export default class Digital implements DigitalIo {
   }
 
 
-  // private resolveEdge(edge?: Edge): number {
-  //   if (edge === 'rising') {
-  //     return Gpio.RISING_EDGE;
-  //   }
-  //   else if (edge === 'falling') {
-  //     return Gpio.FALLING_EDGE;
-  //   }
-  //
-  //   return Gpio.EITHER_EDGE;
-  // }
+  private convertMode(pinMode: DigitalPinMode): {mode: number, pullUpDown: number} {
+    switch (pinMode) {
+      case ('input'):
+        return {
+          mode: Gpio.INPUT,
+          pullUpDown: Gpio.PUD_OFF,
+        };
+      case ('input_pullup'):
+        return {
+          mode: Gpio.INPUT,
+          pullUpDown: Gpio.PUD_UP,
+        };
+      case ('input_pulldown'):
+        return {
+          mode: Gpio.INPUT,
+          pullUpDown: Gpio.PUD_DOWN,
+        };
+      case ('output'):
+        return {
+          mode: Gpio.OUTPUT,
+          pullUpDown: Gpio.PUD_OFF,
+        };
+      default:
+        throw new Error(`Unknown mode "${pinMode}"`);
+    }
+  }
 
-  private getPinInstance(methodWhichAsk: string, pin: number): any {
+  private resolveEdge(edge?: Edge): number {
+    if (edge === 'rising') {
+      return Gpio.RISING_EDGE;
+    }
+    else if (edge === 'falling') {
+      return Gpio.FALLING_EDGE;
+    }
+
+    return Gpio.EITHER_EDGE;
+  }
+
+  private getPinInstance(methodWhichAsk: string, pin: number): Gpio {
     if (!this.pinInstances[pin]) {
       throw new Error(`Digital dev ${methodWhichAsk}: You have to do setup of local GPIO pin "${pin}" before manipulating it`);
     }
