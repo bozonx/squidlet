83871d2f5f683b31b586c5eeb19ca09b6d1fd136
Ivan <ipkozyrin@gmail.com>
2019-05-02 09:09:47 +1000

    add

--- entities/drivers/WebSocketServerRouter/WebSocketServerRouter.ts
+++ entities/drivers/WebSocketServerRouter/WebSocketServerRouter.ts
@@ -2,6 +2,7 @@ import DriverBase from 'system/baseDrivers/DriverBase';
 import {mergeDeep} from '../../../system/helpers/collections';
 import {WebSocketServer, WebSocketServerDriverProps} from '../WebSocketServer/WebSocketServer';
 import WsServerRouterLogic from './WsServerRouterLogic';
+import DriverInstance from 'system/interfaces/DriverInstance';
 
 
 export interface WebSocketServerRouterPropsDriverProps extends WebSocketServerDriverProps {
@@ -11,10 +12,10 @@ export interface WebSocketServerRouterPropsDriverProps extends WebSocketServerDr
 export default class WebSocketServerRouter extends DriverBase {
   async getInstance(instanceProps: {[index: string]: any} = {}): Promise<WsServerRouterLogic> {
     const props: WebSocketServerRouterPropsDriverProps = mergeDeep(instanceProps, this.definition.props) as any;
-
-    const serverDriver: WebSocketServer = this.env.getDriver<WebSocketServer>('WebSocketServer').getInstance(props);
+    const serverDriver: WebSocketServer = this.env.getDriver<DriverInstance>('WebSocketServer')
+      .getInstance(props);
     const routerLogic: WsServerRouterLogic = new WsServerRouterLogic(serverDriver);
 
-    return routerLogic
+    return routerLogic;
   }
 }
--- entities/drivers/WebSocketServerRouter/WsServerRouterLogic.ts
+++ entities/drivers/WebSocketServerRouter/WsServerRouterLogic.ts
@@ -1,10 +1,66 @@
 import {WebSocketServer} from '../WebSocketServer/WebSocketServer';
 
 
+// TODO: make interface of server driver to use just logic
+
+
+interface RouteParams {
+
+}
+
+
+export class RouteConnection {
+  // client's connection to this route
+  readonly connectionId: string;
+  readonly action: string;
+
+
+  constructor() {
+
+  }
+
+  destroy() {
+
+  }
+
+
+  /**
+   * Send to client of this route
+   */
+  send(data: string | Uint8Array): Promise<void> {
+    // TODO: how to send to route ????
+  }
+
+  onMessage(cb: OnMessageHandler): number {
+    // TODO: !!!!
+  }
+
+  removeMessageListener(handlerId: number) {
+    // TODO: !!!!
+  }
+
+}
+
+
 export default class WsServerRouterLogic {
   constructor(serverDriver: WebSocketServer) {
 
   }
 
+
+  onConnection(cb: (routeParams: RouteParams) => void): number {
+
+  }
+
+  removeConnectionListener(handlerId: number) {
+
+  }
+
+  /**
+   * Force closing a connection to all the routes
+   */
+  closeConnection(connectionId: string, code: number, reason: string) {
+
+  }
 
 }
