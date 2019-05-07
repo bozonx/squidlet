da7985d5512693ef54810b9b1aaeedd8285a5296
Ivan <ipkozyrin@gmail.com>
2019-05-08 08:21:04 +1000

    refactor

--- control/resolveCommand.ts
+++ control/resolveCommand.ts
@@ -10,37 +10,24 @@ const remoteEvents = new RemoteEvents();
 
 
 async function startCommand(command: string, positionArgsRest: string[], args: {[index: string]: any}) {
-  if (command === 'start') {
-    const commandUpdate: CommandStart = new CommandStart(positionArgsRest, args as any);
-
-    return commandUpdate.start();
+  switch (command) {
+    case 'start':
+      return (new CommandStart(positionArgsRest, args as any)).start();
+    case 'update':
+      return (new CommandUpdate(positionArgsRest, args)).start();
+    case 'log':
+      return remoteEvents.listenEvent({ ...args, category: 'logger', topic: undefined });
+    case 'pub':
+      return remoteEvents.emitEvent(args);
+    case 'sub':
+      return remoteEvents.listenEvent(args);
+    case 'block-io':
+      // TODO: review
+      return remoteEvents.emitEvent({ ...args, category: 'system', topic: 'block-io' });
+    default:
+      console.error(`Unknown command "${command}"`);
+      process.exit(2);
   }
-  else if (command === 'update') {
-    const commandUpdate: CommandUpdate = new CommandUpdate(positionArgsRest, args);
-
-    return commandUpdate.start();
-  }
-  else if (command === 'log') {
-    await remoteEvents.listenEvent({ ...args, category: 'logger', topic: undefined });
-  }
-  else if (command === 'pub') {
-    await remoteEvents.emitEvent(args);
-  }
-  else if (command === 'sub') {
-    await remoteEvents.listenEvent(args);
-  }
-  else if (command === 'block-io') {
-    // TODO: review
-    await remoteEvents.emitEvent({ ...args, category: 'system', topic: 'block-io' });
-  }
-  // else if (command === 'io-server') {
-  //   const commandUpdate: CommandIoServer = new CommandIoServer(positionArgsRest, args);
-  //
-  //   return commandUpdate.start();
-  // }
-
-  console.error(`Unknown command "${command}"`);
-  process.exit(2);
 }
 
 
