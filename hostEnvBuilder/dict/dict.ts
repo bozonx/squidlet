f0539946842358368afb951b5c524ba5d0b8e05c
Ivan <ipkozyrin@gmail.com>
2019-05-11 11:38:08 +1000

    change

--- hostEnvBuilder/dict/dict.ts
+++ hostEnvBuilder/dict/dict.ts
@@ -1,16 +1,18 @@
 export const servicesShortcut: {[index: string]: string} = {
   automation: 'Automation',
-  mqtt: 'Mqtt',
   consoleLogger: 'ConsoleLogger',
-  backdoor: 'Backdoor',
-  updater: 'Updater',
   ioSetServer: 'IoSetServer',
+  updater: 'Updater',
+
+  mqtt: 'Mqtt',
+  backdoor: 'Backdoor',
 };
 
 // services which set by default event you don't specify it in host config.
 // To turn off the service you should specify: logger: null or backdoor: null.
 export const defaultServices = [
+  //'automation',
   'consoleLogger',
-  'backdoor',
-  'updater',
+  'ioSetServer',
+  //'updater',
 ];
