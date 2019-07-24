e2f1f07dc2a38ff629de4736ecf450ad96e8d669
Ivan <ipkozyrin@gmail.com>
2019-07-24 12:14:33 +1000

    add

--- system/helpers/RequestQueue.ts
+++ system/helpers/RequestQueue.ts
@@ -1,6 +1,9 @@
 type RequestCb = () => void;
-type UniqId = string | undefined;
-type Job = [UniqId, RequestCb];
+type JobId = string;
+type Job = [JobId, RequestCb];
+
+
+const unnamedJobId = 0;
 
 
 export default class RequestQueue {
@@ -15,32 +18,55 @@ export default class RequestQueue {
     return this.jobs.length;
   }
 
-  isJobInProgress(uniqId: UniqId): boolean {
+  isJobInProgress(jobId: JobId): boolean {
+    // TODO: !!!!
+  }
+
+  /**
+   * Check if queue has job with specified jobId even it is a current job.
+   */
+  hadJob(jobId: JobId): boolean {
+    // TODO: !!!!
+  }
+
+  /**
+   * Returns id of current job of undefined
+   */
+  whichIsCurrentJob(): JobId | undefined {
     // TODO: !!!!
   }
 
   /**
    * Cancel current and delayed job with uniq id.
    */
-  cancelJob(uniqId: UniqId) {
+  cancelJob(jobId: JobId) {
+    // TODO: !!!!
+  }
+  
+  waitJobFinished(jobId: JobId): Promise<void> {
     // TODO: !!!!
   }
 
   /**
    * Add job to queue.
-   * If job with the same uniqId is in progress it will put to queue.
+   * If job with the same jobId is in progress it will put to queue.
    * It there is queued delayed job - it just replace the callback with a new one.
    */
-  request(uniqId: UniqId, cb: RequestCb): Promise<void> {
+  request(jobId: JobId | undefined, cb: RequestCb): JobId {
     // TODO: !!!!
   }
 
-  onJobStart(cb: (uniqId: UniqId) => void) {
+  onJobStart(cb: (jobId: JobId) => void) {
     // TODO: !!!!
   }
 
-  onJobEnd(cb: (error: Error | undefined, uniqId: UniqId) => void) {
+  onJobEnd(cb: (error: Error | undefined, jobId: JobId) => void) {
     // TODO: !!!!
   }
 
+  
+  private resolveJobId() {
+    
+  }
+  
 }
