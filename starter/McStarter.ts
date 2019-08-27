3983d457dbfe7682c39b0cf1fa688f1b6f3db7a8
Ivan <ipkozyrin@gmail.com>
2019-08-27 10:25:42 +1000

    add

--- starter/McStarter.ts
+++ starter/McStarter.ts
@@ -1,85 +1,75 @@
-
-
-// TODO: remove
-
-
-import {SystemClassType} from '../shared/interfaces/SystemClassType';
 import IoSetLocal from './IoSetLocal';
 import IoSet from '../system/interfaces/IoSet';
-import IoServer from '../shared/IoServer';
-import StorageIo from '../system/interfaces/io/StorageIo';
-import systemConfig from '../system/config/systemConfig';
-import {IO_SERVER_MODE_FILE_NAME} from '../system/constants';
-import HostConfig from '../system/interfaces/HostConfig';
-import InitializationConfig from '../system/interfaces/InitializationConfig';
-import initializationConfig from '../system/config/initializationConfig';
-import {pathJoin} from '../system/lib/nodeLike';
+import AppSwitcher from '../shared/AppSwitcher';
+import SysIo from '../system/interfaces/io/SysIo';
 
 
+/**
+ * Starter for micro-controllers.
+ */
 class McStarter {
-  private pathToBundle: string;
   private readonly ioSet: IoSet;
+  private readonly appSwitcher: AppSwitcher;
 
 
-  constructor(pathToBundle = `${systemConfig.rootDirs.envSet}/${systemConfig.envSetDirs.system}/System`) {
-    this.pathToBundle = pathToBundle;
+  constructor() {
     this.ioSet = new IoSetLocal();
+    this.appSwitcher = new AppSwitcher(this.ioSet, this.onRestartRequest);
   }
 
 
   async start() {
-    if (await this.isIoServerMode()) {
-      await this.startIoServer();
-    }
-    else {
-      await this.startSystem();
-    }
+    await this.appSwitcher.start();
+
+    // TODO: ??? init ioSet ??
+    // TODO: ??? configure ioSet with config ??
   }
 
 
-  private async isIoServerMode(): Promise<boolean> {
+  private onRestartRequest = () => {
+    const sysIo = this.ioSet.getIo<SysIo>('Sys');
 
-    // TODO: не будет рабоать пока не будет выполнен ioSet.init() !!!!
-
-    const storage = this.ioSet.getIo<StorageIo>('Storage');
-
-    return storage.exists(this.getMarkFilePath());
-  }
-
-  private async startSystem() {
-    const SystemClass: SystemClassType = require(this.pathToBundle).default;
-    const system = new SystemClass(this.ioSet);
-
-    await system.start();
-  }
-
-  private async startIoServer() {
-    const storage = this.ioSet.getIo<StorageIo>('Storage');
-    // remove mark file first
-    await storage.unlink(this.getMarkFilePath());
-
-    const hostConfig = await this.loadHostConfig();
-    const ioServer = new IoServer(this.ioSet, hostConfig, console.info, console.error);
-
-    await ioServer.init();
-  }
-
-  private getMarkFilePath(): string {
-    return `${systemConfig.rootDirs.tmp}/${IO_SERVER_MODE_FILE_NAME}`;
-  }
-
-  private async loadHostConfig(): Promise<HostConfig> {
-    const initCfg: InitializationConfig = initializationConfig();
-    const pathToConfig = pathJoin(
-      systemConfig.rootDirs.envSet,
-      systemConfig.envSetDirs.configs,
-      initCfg.fileNames.hostConfig
-    );
-
-    const storage = this.ioSet.getIo<StorageIo>('Storage');
-    const configStr: string = await storage.readFile(pathToConfig);
-
-    return JSON.parse(configStr);
+    sysIo.reboot()
+      .catch(console.error);
   }
 
 }
+
+//
+// private async isIoServerMode(): Promise<boolean> {
+//
+//   // T-O-D-O: не будет рабоать пока не будет выполнен ioSet.init() !!!!
+//
+//   const storage = this.ioSet.getIo<StorageIo>('Storage');
+//
+//   return storage.exists(this.getMarkFilePath());
+// }
+
+// private async startIoServer() {
+//   const storage = this.ioSet.getIo<StorageIo>('Storage');
+//   // remove mark file first
+//   await storage.unlink(this.getMarkFilePath());
+//
+//   const hostConfig = await this.loadHostConfig();
+//   const ioServer = new IoServer(this.ioSet, hostConfig, console.info, console.error);
+//
+//   await ioServer.init();
+// }
+
+// private getMarkFilePath(): string {
+//   return `${systemConfig.rootDirs.tmp}/${IO_SERVER_MODE_FILE_NAME}`;
+// }
+
+// private async loadHostConfig(): Promise<HostConfig> {
+//   const initCfg: InitializationConfig = initializationConfig();
+//   const pathToConfig = pathJoin(
+//     systemConfig.rootDirs.envSet,
+//     systemConfig.envSetDirs.configs,
+//     initCfg.fileNames.hostConfig
+//   );
+//
+//   const storage = this.ioSet.getIo<StorageIo>('Storage');
+//   const configStr: string = await storage.readFile(pathToConfig);
+//
+//   return JSON.parse(configStr);
+// }
