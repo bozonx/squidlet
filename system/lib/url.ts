3b9d2d265c9a3187762709a68219b8582ace93a1
Ivan <ipkozyrin@gmail.com>
2019-09-05 09:16:54 +1000

    add

--- system/lib/url.ts
+++ system/lib/url.ts
@@ -21,23 +21,28 @@ function parseSearch(rawSearch: string): {[index: string]: JsonTypes} {
   // TODO: add - see cookies
 }
 
-function parseHostPort(rasStr: string): { host: string, port?: number } {
+export function parseHostPort(rawStr: string): { host: string, port?: number } {
+  if (!rawStr) {
+    throw new Error(`Invalid host part of url`);
+  }
 
+  const splat = rawStr.split(':');
+
+  if (splat.length > 1) {
+    const portNum: number = Number(splat[2]);
+
+    if (Number.isNaN(portNum)) throw new Error(`Invalid port number`);
+
+    return { host: splat[1], port: portNum };
+  }
+
+  return { host: splat[1] };
 }
 
-function parseUserPassword(rasStr: string): {user?: string; password?: string} {
+export function parseUserPassword(rawStr: string): {user?: string; password?: string} {
+  if (!rawStr) return {};
 
-}
 
-function parseAuthHostPart(rawStr: string): {host: string; port?: number; user?: string; password?: string} {
-  if (!rawStr) throw new Error(`Invalid auth and host part of url`);
-
-  const splat = rawStr.split('@');
-
-  return {
-    ...parseHostPort(splat[(splat.length === 2) ? 2 : 1]),
-    ...(splat.length === 2) ? parseUserPassword(splat[1]) : undefined,
-  };
 }
 
 export function parseUrl(rawUrl: string): ParsedUrl {
@@ -45,12 +50,20 @@ export function parseUrl(rawUrl: string): ParsedUrl {
 
   const match = rawUrl.trim().match(/^([\w\d]+):\/\/([^\/]+)([^?]*)\??(.*)$/);
 
-  if (!match) throw new Error(`Can't parse url "${rawUrl}"`);
+  if (!match) {
+    throw new Error(`Can't parse url "${rawUrl}"`);
+  }
+  else if (!match[2]) {
+    throw new Error(`Invalid auth and host part of url`);
+  }
+
+  const authHostSplat = match[2].split('@');
 
   return {
     protocol: match[1],
     url: match[3],
     search: parseSearch(match[4]),
-    ...parseAuthHostPart(match[2]),
+    ...parseHostPort(authHostSplat[(authHostSplat.length === 2) ? 2 : 1]),
+    ...(authHostSplat.length === 2) ? parseUserPassword(authHostSplat[1]) : undefined,
   };
 }
