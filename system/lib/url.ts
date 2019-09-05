ae5107a1d073de6f67485837ccb6b1bed433867f
Ivan <ipkozyrin@gmail.com>
2019-09-05 10:02:23 +1000

    test

--- system/lib/url.ts
+++ system/lib/url.ts
@@ -1,20 +1,44 @@
 import {JsonTypes} from '../interfaces/Types';
-import {parseValue} from './common';
 
 
 export const URL_DELIMITER = '/';
 
 export interface ParsedUrl {
-  protocol: string;
+  // protocol
+  scheme: string;
   user?: string;
   password?: string;
   host: string;
   port?: number;
   // relative part of url e.g / or /page/ or empty string
-  url: string;
+  path: string;
   search: {[index: string]: JsonTypes};
 }
 
+
+/**
+ * Parses value of search param.
+ * Pass it already url decoded.
+ * It supports arrays and objects in JSON format like:
+ * * param1=[1, true, "str"]
+ * * param1={"a": "str", "b": 5, "c": true}
+ * Params which defined without value will have an empty string as a value according to URLSearchParams:
+ * * param1&param2=2 => { param1: '', param2: 2 }
+ */
+export function parseSearchValue(rawValue: string | undefined): JsonTypes {
+  if (!rawValue) return '';
+
+  const trimmed: string = rawValue.trim();
+
+  if (!trimmed) return '';
+
+  try {
+    return JSON.parse(trimmed);
+  }
+  catch (e) {
+    return trimmed;
+  }
+}
 
 export function parseSearch(rawSearch: string): {[index: string]: JsonTypes} {
   if (!rawSearch) return {};
@@ -25,9 +49,7 @@ export function parseSearch(rawSearch: string): {[index: string]: JsonTypes} {
   for (let item of splat) {
     const [key, value] = item.split('=');
 
-    // TODO: support of lists
-
-    result[key.trim()] = parseValue((value || '').trim() || undefined);
+    result[key.trim()] = parseSearchValue(value);
   }
 
   return result;
@@ -72,10 +94,11 @@ export function parseUserPassword(rawStr: string): {user?: string; password?: st
 export function parseUrl(rawUrl: string): ParsedUrl {
   if (!rawUrl) throw new Error(`Invalid url "${rawUrl}"`);
 
-  const match = rawUrl.trim().match(/^([\w\d]+):\/\/([^\/]+)([^?]*)\??(.*)$/);
+  const decodedUrl = decodeURI(rawUrl);
+  const match = decodedUrl.trim().match(/^([\w\d]+):\/\/([^\/]+)([^?]*)\??(.*)$/);
 
   if (!match) {
-    throw new Error(`Can't parse url "${rawUrl}"`);
+    throw new Error(`Can't parse url "${decodedUrl}"`);
   }
   else if (!match[2]) {
     throw new Error(`Invalid auth and host part of url`);
@@ -84,8 +107,8 @@ export function parseUrl(rawUrl: string): ParsedUrl {
   const authHostSplat = match[2].split('@');
 
   return {
-    protocol: match[1],
-    url: match[3],
+    scheme: match[1],
+    path: match[3],
     search: parseSearch(match[4]),
     ...parseHostPort(authHostSplat[(authHostSplat.length === 2) ? 1 : 0]),
     ...(authHostSplat.length === 2) ? parseUserPassword(authHostSplat[0]) : {},
--- test/system/lib/url.spec.coffee
+++ test/system/lib/url.spec.coffee
@@ -1,42 +1,53 @@
 url = require('../../../system/lib/url')
 
 
-describe.only 'system.lib.url', ->
+describe 'system.lib.url', ->
   it 'parseSearch', ->
     assert.deepEqual(url.parseSearch('param1=value1&param2&param3=5&param4=true'), {
       param1: 'value1',
-      param2: undefined,
+      param2: '',
       param3: 5,
       param4: true,
     })
     assert.deepEqual(url.parseSearch('param1=[1, true, "str"]'), {
       param1: [1, true, 'str']
     })
-    # TODO: test lists
+    assert.deepEqual(url.parseSearch('param1={"a": "str", "b": 5, "c": true}'), {
+      param1: {a: "str", b: 5, c: true}
+    })
   it 'parseHostPort', ->
     assert.deepEqual(url.parseHostPort('pre.host.com:8080'), {
       host: 'pre.host.com',
       port: 8080,
     })
-    # TODO: test other cases
+    assert.deepEqual(url.parseHostPort('pre.host.com'), {
+      host: 'pre.host.com',
+    })
+    assert.throws(() => url.parseHostPort(''))
+    assert.throws(() => url.parseHostPort('host:port'))
+    assert.throws(() => url.parseHostPort('host:80:90'))
 
   it 'parseUserPassword', ->
     assert.deepEqual(url.parseUserPassword('username:password'), {
       user: 'username',
       password: 'password',
     })
-    # TODO: test other cases
+    assert.deepEqual(url.parseUserPassword('username'), {
+      user: 'username',
+    })
+    assert.deepEqual(url.parseUserPassword(''), {})
+    assert.throws(() => url.parseHostPort('username:password:other'))
 
   it 'parseUrl', ->
     testUrl = 'https://username:password@host.com:8080/path/to/route/?param1=value1&param2'
     assert.deepEqual(url.parseUrl(testUrl), {
-      protocol: 'https',
+      scheme: 'https',
       host: 'host.com',
       port: 8080,
-      url: '/path/to/route/',
+      path: '/path/to/route/',
       search: {
         param1: 'value1',
-        param2: undefined,
+        param2: '',
       },
       user: 'username',
       password: 'password',
