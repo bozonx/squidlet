16dc057be9fd1b47cb9d42126c14657e3872fb95
Ivan <ipkozyrin@gmail.com>
2019-09-13 08:56:19 +1000

    add automation rule parse

--- entities/services/Automation/Automation.ts
+++ entities/services/Automation/Automation.ts
@@ -7,13 +7,39 @@ interface Props {
   rules: RuleDefinition[];
 }
 
+let nameIndex: number = 0;
+
 
 export default class Automation extends ServiceBase<Props> {
   private readonly rules: RuleItem[] = [];
 
 
-  protected willInit = async () => {
+  protected appDidInit = async () => {
+    this.prepareRules();
+  }
 
+
+  private prepareRules() {
+    for (let ruleDefinition of this.props.rules) {
+      this.validateRule(ruleDefinition);
+
+      const ruleItem: RuleItem = {
+        name: ruleDefinition.name || this.generateQniqName(),
+      };
+
+      this.rules.push(ruleItem);
+    }
+  }
+
+
+  private validateRule(ruleDefinition: RuleDefinition) {
+    // TODO: validate rule definiton
+  }
+
+  private generateQniqName(): string {
+    nameIndex++;
+
+    return String(nameIndex);
   }
 
 }
