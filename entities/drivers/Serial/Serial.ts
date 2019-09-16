21474c4a7cf563c781f47d19b10018e0dc61c9db
Ivan <ipkozyrin@gmail.com>
2019-09-16 11:17:47 +1000

    add

--- entities/drivers/Serial/Serial.ts
+++ entities/drivers/Serial/Serial.ts
@@ -1,10 +1,51 @@
-import DriverFactoryBase from '../../../system/base/DriverFactoryBase';
-import {SerialDuplex, SerialDuplexProps} from '../SerialDuplex/SerialDuplex';
-import DuplexDriver from '../../../system/interfaces/DuplexDriver';
-import DriverBase from '../../../system/base/DriverBase';
+import DriverFactoryBase from 'system/base/DriverFactoryBase';
+import DriverBase from 'system/base/DriverBase';
+import SerialIo, {SerialParams} from 'system/interfaces/io/SerialIo';
+import {omitObj} from 'system/lib/objects';
 
 
-export class Serial extends DriverBase<SerialDuplexProps> implements DuplexDriver {
+interface Props extends SerialParams {
+  portNum: number;
+}
+
+
+export class Serial extends DriverBase<Props> {
+  private get serialIo(): SerialIo {
+    return this.getIo('Serial') as any;
+  }
+  private portNum: number = -1;
+
+
+  protected willInit = async () => {
+    this.portNum = await this.serialIo.newPort(
+      this.props.portNum,
+      omitObj(this.props, 'portNum')
+    );
+
+    await this.serialIo.onError(this.portNum, this.log.error);
+    await this.serialIo.onData(this.portNum, this.handleData);
+
+    this.log.debug(`Serial driver: Connected to port number ${this.portNum}`);
+  }
+
+
+  destroy = async () => {
+    // TODO: !!!
+  }
+
+
+  onMessage(cb: (portNum: number, data: string | Uint8Array) => void): number {
+    // TODO: !!!
+  }
+
+  removeMessageListener(handlerIndex: number) {
+    // TODO: !!!
+  }
+
+
+  private handleData(data: string | Uint8Array) {
+
+  }
 
 }
 
