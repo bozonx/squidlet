b9b8613615c32fd9ef99aa4912c12dc598e0cf78
Ivan <ipkozyrin@gmail.com>
2019-09-17 13:12:41 +1000

    add

--- shared/buildToJs/rollupToOneFile.ts
+++ shared/buildToJs/rollupToOneFile.ts
@@ -1,29 +1,78 @@
 import * as rollup from 'rollup';
 import {ModuleFormat} from 'rollup';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import * as sourceMaps from 'rollup-plugin-sourcemaps';
+import * as json from 'rollup-plugin-json';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import {OutputOptions} from 'rollup';
+
+const babel = require('rollup-plugin-babel');
+
+
+function makePlugins(useSourceMaps?: boolean, minimize?: boolean): any[] {
+  return [
+    // // replace config
+    // replace({
+    //   __BUILD_CONFIG__: JSON.stringify({
+    //     envName: process.env.ENV_CONF,
+    //     NODE_ENV: process.env.NODE_ENV,
+    //     envConfig,
+    //   })
+    // }),
+    typescript({
+      useTsconfigDeclarationDir: true,
+      tsconfigOverride: {
+        compilerOptions: {
+          module: 'ESNext',
+        }
+      }
+    }),
+    // Allow json resolution
+    json(),
+
+    // Allow node_modules resolution
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    babel({
+      extensions: ['.ts', '.js'],
+    }),
+    // Allow bundling cjs modules
+    resolve({
+      extensions: ['.ts', '.js'],
+    }),
+    // Resolve source maps to the original source
+    useSourceMaps && sourceMaps(),
+    // minimize
+    minimize && terser(),
+  ];
+}
 
 
 export default async function(
   name: string,
   indexFilePath: string,
   outputFilePath: string,
-  globals?: string[],
+  globals?: {[index: string]: string},
   external?: string[],
-  sourceMaps?: boolean,
+  useSourceMaps?: boolean,
   minimize?: boolean,
 ) {
-  // TODO: use minimize
   const inputOptions = {
     input: indexFilePath,
     external,
+    plugins: makePlugins(useSourceMaps, minimize),
   };
   // create a bundle
   const bundle = await rollup.rollup(inputOptions);
   const format: ModuleFormat = 'umd';
-  const outputOptions = {
+  const outputOptions: OutputOptions = {
     name,
     file: outputFilePath,
     format,
-    sourcemap: sourceMaps,
+    sourcemap: useSourceMaps,
     globals,
   };
 
