555b75a9ebaf1cf96faa3302625cd00ab91c5ce2
Ivan <ipkozyrin@gmail.com>
2019-09-17 11:48:00 +1000

    change

--- squidletLight/LightBuilder.ts
+++ squidletLight/LightBuilder.ts
@@ -8,6 +8,7 @@ import {ENV_SET_GLOBAL_CONST, IOS_GLOBAL_CONST, MAIN_FILES_GLOBAL_CONST} from '.
 import Platforms from '../system/interfaces/Platforms';
 import HostEnvSet from '../hostEnvBuilder/interfaces/HostEnvSet';
 import HostEntitySet from '../hostEnvBuilder/interfaces/HostEntitySet';
+import {EntityTypePlural} from '../system/interfaces/EntityTypes';
 
 
 export default class LightBuilder {
@@ -41,21 +42,31 @@ export default class LightBuilder {
     console.info(`===> collect env set`);
     await this.envBuilder.collect();
 
-    const indexFileStr: string = await this.makeIndexFile();
     const indexFilePath = path.join(this.workDir, 'index.ts');
+    const indexFileStr: string = await this.makeIndexFile();
+    const iosFilePath: string = path.join(this.workDir, 'ios.ts');
+    const iosFileStr: string = await this.prepareIoClassesString();
+    const devicesFilePath: string = path.join(this.workDir, 'devices.ts');
+    const devicesFileStr: string = await this.makeEntitiesMainFilesString('devices');
+    const driversFilePath: string = path.join(this.workDir, 'drivers.ts');
+    const driversFileStr: string = await this.makeEntitiesMainFilesString('drivers');
+    const servicesFilePath: string = path.join(this.workDir, 'services.ts');
+    const servicesFileStr: string = await this.makeEntitiesMainFilesString('services');
 
     await this.os.writeFile(indexFilePath, indexFileStr);
+    await this.os.writeFile(iosFilePath, iosFileStr);
+    await this.os.writeFile(devicesFilePath, devicesFileStr);
+    await this.os.writeFile(driversFilePath, driversFileStr);
+    await this.os.writeFile(servicesFilePath, servicesFileStr);
 
     await this.rollup();
   }
 
 
   private async makeIndexFile(): Promise<string> {
-    return this.prepareIoClassesString()
-      + '\n\n'
-      + this.prepareEnvSetString()
-      + '\n\n'
-      + this.prepareMainFilesString()
+    // TODO: add System starter import
+    // TODO: add ioSet import
+    return this.prepareEnvSetString()
       + '\n';
   }
 
@@ -64,7 +75,7 @@ export default class LightBuilder {
     // TODO: лучше брать только те что реально используются
     const machineIosList = this.envBuilder.configManager.machineConfig.ios;
     let imports = '';
-    let exportStr = `\nglobal.${IOS_GLOBAL_CONST} = {\n`;
+    let exportStr = `// @ts-ignore\nglobal.${IOS_GLOBAL_CONST} = {\n`;
 
     // make imports
     for (let ioPath of machineIosList) {
@@ -78,29 +89,30 @@ export default class LightBuilder {
       exportStr += `  ${ioName},\n`;
     }
 
-    return imports + exportStr + `};`;
+    return `${imports}\n${exportStr}};`;
   }
 
   private prepareEnvSetString(): string {
     const envSetStr = JSON.stringify(this.envBuilder.generateProdEnvSet(), null,2);
 
-    return `global.${ENV_SET_GLOBAL_CONST} = ${envSetStr}`;
+    return `// @ts-ignore\nglobal.${ENV_SET_GLOBAL_CONST} = ${envSetStr}`;
   }
 
-  private prepareMainFilesString(): string {
+  private makeEntitiesMainFilesString(pluralName: EntityTypePlural): string {
     const envSet: HostEnvSet = this.envBuilder.generateDevelopEnvSet();
+    const entities: {[index: string]: HostEntitySet} = envSet.entities[pluralName];
     let imports = '';
-    let exportStr = `\nglobal.${MAIN_FILES_GLOBAL_CONST} = {\n`;
+    let exportStr = `export default {\n`;
 
-    const eachEntity = (entities: {[index: string]: HostEntitySet}) => {
-      // TODO: add
-    };
+    for (let entityName of Object.keys(entities)) {
+      const srcAbsDir: string = entities[entityName].srcDir;
+      const fileName = path.join(srcAbsDir, entities[entityName].manifest.main);
 
-    eachEntity(envSet.entities.devices);
-    eachEntity(envSet.entities.drivers);
-    eachEntity(envSet.entities.services);
+      imports += this.makeImportString(entities[entityName].manifest.name, fileName);
+      exportStr += `  ${entities[entityName].manifest.name},\n`;
+    }
 
-    return imports + exportStr + `};`;
+    return `${imports}\n${exportStr}};`;
   }
 
   private async rollup() {
--- squidletLight/constants.ts
+++ squidletLight/constants.ts
@@ -1,3 +1,3 @@
-export const IOS_GLOBAL_CONST = '__IOS_SET__';
+//export const IOS_GLOBAL_CONST = '__IOS_SET__';
 export const ENV_SET_GLOBAL_CONST = '__ENV_SET__';
-export const MAIN_FILES_GLOBAL_CONST = '__MAIN_FILES__';
+//export const MAIN_FILES_GLOBAL_CONST = '__MAIN_FILES__';
