c7bc69037ca63ef962ab9cb3f6352a57c7df6df3
Ivan <ipkozyrin@gmail.com>
2019-09-17 12:04:52 +1000

    add

--- squidletLight/LightBuilder.ts
+++ squidletLight/LightBuilder.ts
@@ -4,7 +4,6 @@ import EnvBuilder from '../hostEnvBuilder/EnvBuilder';
 import {HOST_TMP_DIR} from '../shared/constants';
 import Os from '../shared/Os';
 import {getFileNameOfPath, removeExtFromFileName} from '../shared/helpers';
-import {ENV_SET_GLOBAL_CONST} from './constants';
 import Platforms from '../system/interfaces/Platforms';
 import HostEnvSet from '../hostEnvBuilder/interfaces/HostEnvSet';
 import HostEntitySet from '../hostEnvBuilder/interfaces/HostEntitySet';
@@ -48,6 +47,8 @@ export default class LightBuilder {
     const indexFileStr: string = await this.makeIndexFile();
     const iosFilePath: string = path.join(this.workDir, 'ios.ts');
     const iosFileStr: string = await this.prepareIoClassesString();
+    const envSetPath: string = path.join(this.workDir, 'envSet.ts');
+    const envSetStr: string = await this.prepareEnvSetString();
     const devicesFilePath: string = path.join(this.workDir, 'devicesMainFiles.ts');
     const devicesFileStr: string = await this.makeEntitiesMainFilesString('devices');
     const driversFilePath: string = path.join(this.workDir, 'driversMainFiles.ts');
@@ -57,6 +58,7 @@ export default class LightBuilder {
 
     await this.os.writeFile(indexFilePath, indexFileStr);
     await this.os.writeFile(iosFilePath, iosFileStr);
+    await this.os.writeFile(envSetPath, envSetStr);
     await this.os.writeFile(devicesFilePath, devicesFileStr);
     await this.os.writeFile(driversFilePath, driversFileStr);
     await this.os.writeFile(servicesFilePath, servicesFileStr);
@@ -66,10 +68,11 @@ export default class LightBuilder {
 
 
   private async makeIndexFile(): Promise<string> {
+    return '';
     // TODO: add System starter import
     // TODO: add ioSet import
-    return this.prepareEnvSetString()
-      + '\n';
+    // return this.prepareEnvSetString()
+    //   + '\n';
   }
 
   private prepareIoClassesString(): string {
@@ -94,7 +97,7 @@ export default class LightBuilder {
   private prepareEnvSetString(): string {
     const envSetStr = JSON.stringify(this.envBuilder.generateProdEnvSet(), null,2);
 
-    return `// @ts-ignore\nglobal.${ENV_SET_GLOBAL_CONST} = ${envSetStr}`;
+    return `export default ${envSetStr}`;
   }
 
   private makeEntitiesMainFilesString(pluralName: EntityTypePlural): string {
@@ -104,9 +107,12 @@ export default class LightBuilder {
 
     for (let entityName of Object.keys(entities)) {
       const srcAbsDir: string = entities[entityName].srcDir;
-      const fileName = path.join(srcAbsDir, entities[entityName].manifest.main);
+      const relFileName: string = path.relative(
+        this.workDir,
+        path.join(srcAbsDir, entities[entityName].manifest.main)
+      );
 
-      exportsStr += this.makeExportString(entities[entityName].manifest.name, fileName);
+      exportsStr += this.makeExportString(entities[entityName].manifest.name, relFileName);
     }
 
     return exportsStr;
