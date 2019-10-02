1056bb78dd0d40c1829999394341191eae89c1b4
Ivan <ipkozyrin@gmail.com>
2019-10-03 08:46:53 +1000

    add

--- entities/services/Automation/values/AndValue.ts
+++ entities/services/Automation/values/AndValue.ts
@@ -1,3 +1,16 @@
-export default function () {
+import Context from 'system/Context';
+import ValueDefinition from '../interfaces/ValueDefinition';
 
+
+interface AndDefinition {
+  check: ValueDefinition[];
+}
+
+
+export default function (context: Context, definition: AndDefinition): boolean {
+  for (let valueDefinition of definition.check) {
+
+  }
+
+  return true;
 }
---
+++ entities/services/Automation/values/allValues.ts
@@ -0,0 +1,12 @@
+import StatusBooleanValue from './StatusBooleanValue';
+import StatusValue from './StatusValue';
+import AndValue from './AndValue';
+import OrValue from './OrValue';
+
+
+export default {
+  andValue: AndValue,
+  orValue: OrValue,
+  statusBoolean: StatusBooleanValue,
+  status: StatusValue,
+};
