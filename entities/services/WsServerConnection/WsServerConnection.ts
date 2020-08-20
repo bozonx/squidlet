52b29ef5948e461192bafcae7b18d1ade1f1f599
ivan <ipkozyrin@gmal.com>
2020-08-20 20:38:15 +1000

    add

--- entities/services/WsServerConnection/WsServerConnection.ts
+++ entities/services/WsServerConnection/WsServerConnection.ts
@@ -1,16 +1,20 @@
-import DriverFactoryBase from 'system/base/DriverFactoryBase';
 import Connection, {
-  ChannelIncomeRequestHandler,
-  ChannelRequest,
-  ChannelSendResponse
+  ConnectionIncomeRequestHandler,
+  ConnectionIncomeResponseHandler,
+  ConnectionRequest,
+  ConnectionResponse,
+  ConnectionStatus
 } from 'system/interfaces/Connection';
-import {WsServer} from '../WsServer/WsServer';
-import {ConnectionParams} from '../../../system/interfaces/io/WebSocketServerIo';
-import {WsServerSessions} from '../WsServerSessions/WsServerSessions';
-import IndexedEvents from '../../../system/lib/IndexedEvents';
-import ServiceBase from '../../../system/base/ServiceBase';
-import {WsServerSessionsProps} from '../../drivers/WsServerSessions/WsServerSessions';
-import {makeConnectionRequest, makeConnectionRequestMessage} from '../../../system/lib/connectionHelpers';
+import {ConnectionParams} from 'system/interfaces/io/WebSocketServerIo';
+import IndexedEvents from 'system/lib/IndexedEvents';
+import ServiceBase from 'system/base/ServiceBase';
+import {
+  makeConnectionRequest,
+  makeConnectionRequestMessage,
+  makeConnectionResponseMessage
+} from 'system/lib/connectionHelpers';
+
+import {WsServerSessions, WsServerSessionsProps} from '../../drivers/WsServerSessions/WsServerSessions';
 
 
 interface Props extends WsServerSessionsProps {
@@ -19,8 +23,7 @@ interface Props extends WsServerSessionsProps {
 
 export default class WsServerConnection extends ServiceBase<Props> implements Connection {
   server!: WsServerSessions;
-  // TODO: add type
-  private incomeMessagesEvent = new IndexedEvents();
+  private incomeMessagesEvent = new IndexedEvents<ConnectionIncomeResponseHandler>();
 
 
   init = async () => {
@@ -29,6 +32,7 @@ export default class WsServerConnection extends ServiceBase<Props> implements Co
 
     this.server.onMessage((sessionId: string, data: string | Uint8Array) => {
       // TODO: add
+      // TODO: decode and make response
       // TODO: use incomeMessagesEvent
     });
     this.server.onNewSession((sessionId: string, connectionParams: ConnectionParams) => {
@@ -40,150 +44,66 @@ export default class WsServerConnection extends ServiceBase<Props> implements Co
   }
 
 
-  async request(sessionId: string, channel: number, data: Uint8Array): Promise<ChannelSendResponse> {
-    // TODO: сформировать requestId
-    // TODO: сформировать запрос
-
-    const request: ChannelRequest = makeConnectionRequest(channel, data);
+  async request(sessionId: string, channel: number, data: Uint8Array): Promise<ConnectionResponse> {
+    const request: ConnectionRequest = makeConnectionRequest(channel, data);
     const requestMessage: Uint8Array = makeConnectionRequestMessage(request);
-    // wait while request is finished
+    // send request and wait while sending is finished
     await this.server.send(sessionId, requestMessage);
-
-    // try {
-    //   await this.server.send(sessionId, requestMessage);
-    // }
-    // catch (e) {
-    //   throw e;
-    // }
-    // finally {
-    //   this.incomeMessagesEvent.removeListener(listenHandler);
-    // }
-
+    // wait for response
     return new Promise((resolve, reject) => {
-      const listenHandler = this.incomeMessagesEvent.addListener(() => {
-        // TODO: listen to message from server which is has the same requestId
-        // TODO: decode and make response
-        // TODO: remove listenHandler
+      const handlerIndex = this.incomeMessagesEvent.addListener((
+        response: ConnectionResponse
+      ) => {
+        // listen only our response
+        if (response.requestId !== request.requestId) return;
+
+        this.incomeMessagesEvent.removeListener(handlerIndex);
+
+        // TODO: или может всегда делать resolve ????
+        if (response.status === ConnectionStatus.responseError) {
+          reject(new Error(response.error));
+        }
+        else {
+          resolve(response);
+        }
       });
 
       // TODO: add timeout
     });
   }
 
-  onRequest(channel: number, handler: ChannelIncomeRequestHandler): number {
-    // TODO: передавать session id в handler
+  // TODO: когда сессия закончится удалять всех слушателей данной сессии
+  onRequest(sessionId: string, channel: number, handler: ConnectionIncomeRequestHandler): number {
+    // TODO: обработка ошибок самого cbWrapper
+    // TODO: не известно что это responce или request
+    const cbWrapper = async (request: ConnectionRequest) => {
+      // TODO: как проверить сессию что она наша?
+      // check for our channel
+      if (request.channel !== channel) return;
+
+      // TODO: првоерить что это request
+
+      let response: ConnectionResponse;
+
+      try {
+        response = await handler(request);
+      }
+      catch (e) {
+        // TODO: что делать в лучае ошибки ???
+      }
+
+      const responseMessage: Uint8Array = makeConnectionResponseMessage(response);
+      // TODO: отправить ответ
+      await this.server.send(sessionId, responseMessage);
+    };
+
+    return this.incomeMessagesEvent.addListener(cbWrapper);
+
+    // TODO: add timeout
   }
 
   removeListener(handlerIndex: number): void {
-    // TODO: add
+    this.incomeMessagesEvent.removeListener(handlerIndex);
   }
-
-
-  // // it fulfils when server is start listening
-  // get listeningPromise(): Promise<void> {
-  //   if (!this.server) {
-  //     throw new Error(`WebSocketServer.listeningPromise: ${this.closedMsg}`);
-  //   }
-  //
-  //   return this.server.listeningPromise;
-  // }
-  //
-  // private get wsServerIo(): WebSocketServerIo {
-  //   return this.context.getIo('WebSocketServer') as any;
-  // }
-  // private server?: WsServerLogic;
-  // private get closedMsg() {
-  //   return `Server "${this.props.host}:${this.props.port}" has been already closed`;
-  // }
-  //
-  //
-  // init = async () => {
-  //   this.server = new WsServerLogic(
-  //     this.wsServerIo,
-  //     this.props,
-  //     this.onServerClosed,
-  //     this.log.debug,
-  //     this.log.info,
-  //     this.log.error
-  //   );
-  //
-  //   await this.server.init();
-  // }
-  //
-  // // protected appDidInit = async () => {
-  // //   this.server && await this.server.init();
-  // // }
-  //
-  // destroy = async () => {
-  //   if (!this.server) return;
-  //
-  //   await this.server.destroy();
-  //   delete this.server;
-  // }
-  //
-  //
-  // send(connectionId: string, data: string | Uint8Array): Promise<void> {
-  //   if (!this.server) throw new Error(`WebSocketServer.send: ${this.closedMsg}`);
-  //
-  //   return this.server.send(connectionId, data);
-  // }
-  //
-  // // TODO: add closeServer ???
-  //
-  // /**
-  //  * Force closing a connection
-  //  */
-  // async closeConnection(connectionId: string, code: number, reason: string): Promise<void> {
-  //   if (!this.server) return;
-  //
-  //   await this.server.closeConnection(connectionId, code, reason);
-  // }
-  //
-  // async destroyConnection(connectionId: string) {
-  //   if (!this.server) return;
-  //
-  //   await this.server.destroyConnection(connectionId);
-  // }
-  //
-  // async setCookie(connectionId: string, cookie: string) {
-  //   if (!this.server) return;
-  //
-  //   await this.server.setCookie(connectionId, cookie);
-  // }
-  //
-  // onMessage(cb: (connectionId: string, data: string | Uint8Array) => void): number {
-  //   if (!this.server) throw new Error(`WebSocketServer.onMessage: ${this.closedMsg}`);
-  //
-  //   return this.server.onMessage(cb);
-  // }
-  //
-  // onConnection(
-  //   cb: (connectionId: string, connectionParams: ConnectionParams) => void
-  // ): number {
-  //   if (!this.server) throw new Error(`WebSocketServer.onConnection: ${this.closedMsg}`);
-  //
-  //   return this.server.onConnection(cb);
-  // }
-  //
-  // /**
-  //  * Ordinary connection close.
-  //  * It won't be called on destroy
-  //  */
-  // onConnectionClose(cb: (connectionId: string) => void): number {
-  //   if (!this.server) throw new Error(`WebSocketServer.onConnectionClose: ${this.closedMsg}`);
-  //
-  //   return this.server.onConnectionClose(cb);
-  // }
-  //
-  // removeListener(handlerIndex: number) {
-  //   if (!this.server) return;
-  //
-  //   this.server.removeListener(handlerIndex);
-  // }
-  //
-  //
-  // private onServerClosed = () => {
-  //   this.log.error(`WebSocketServer: ${this.closedMsg}, you can't manipulate it any more!`);
-  // }
 
 }
