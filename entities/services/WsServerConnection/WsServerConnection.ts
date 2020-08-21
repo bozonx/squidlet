df5ffb6309dd357c4a0e31995a995db5cf4074e7
ivan <ipkozyrin@gmal.com>
2020-08-21 10:01:12 +1000

    change

--- entities/services/WsServerConnection/WsServerConnection.ts
+++ entities/services/WsServerConnection/WsServerConnection.ts
@@ -5,6 +5,7 @@ import Connection, {
   ConnectionStatus
 } from 'system/interfaces/Connection';
 import {ConnectionParams} from 'system/interfaces/io/WebSocketServerIo';
+import ConnectionBase from 'system/lib/base/ConnectionBase';
 import IndexedEvents from 'system/lib/IndexedEvents';
 import ServiceBase from 'system/base/ServiceBase';
 import {
@@ -20,24 +21,26 @@ import {WsServerSessions, WsServerSessionsProps} from '../../drivers/WsServerSes
 
 
 type Timeout = NodeJS.Timeout;
-type IncomeRequestHandler = (request: ConnectionRequest, sessionId: string) => void;
-type IncomeResponseHandler = (response: ConnectionResponse, sessionId: string) => void;
+
 
 interface Props extends WsServerSessionsProps {
 }
 
 
-export default class WsServerConnection extends ServiceBase<Props> implements Connection {
+export default class WsServerConnection extends ConnectionBase<Props> {
   server!: WsServerSessions;
-  private incomeRequestsEvent = new IndexedEvents<IncomeRequestHandler>();
-  private incomeResponsesEvent = new IndexedEvents<IncomeResponseHandler>();
+
 
 
   init = async () => {
     // it creates a new server on specified host:port
     this.server = await this.context.getSubDriver('WsServerSessions', this.props);
 
-    this.server.onMessage(this.handleIncomeMessage);
+    this.server.onMessage((sessionId: string, data: string | Uint8Array) => {
+      if (!(data instanceof Uint8Array) || !data.length) return;
+
+      this.handleIncomeMessage(sessionId, data);
+    });
     this.server.onNewSession((sessionId: string, connectionParams: ConnectionParams) => {
       // TODO: add
     });
@@ -46,115 +49,9 @@ export default class WsServerConnection extends ServiceBase<Props> implements Co
     });
   }
 
-
-  async request(sessionId: string, channel: number, data: Uint8Array): Promise<ConnectionResponse> {
-    const request: ConnectionRequest = makeConnectionRequest(channel, data);
-    const requestMessage: Uint8Array = encodeRequest(request);
-    // send request and wait while sending is finished
-    await this.server.send(sessionId, requestMessage);
-
-    const promised = new Promised<ConnectionResponse>();
-    let timeout: Timeout | undefined;
-
-    // wait for response
-    const handlerIndex = this.incomeResponsesEvent.addListener((
-      response: ConnectionResponse
-    ) => {
-      // check if promised if fulfilled in case of timeout has exceeded
-      if (promised.isFulfilled()) return;
-      // listen only our response
-      if (response.requestId !== request.requestId) return;
-
-      // TODO: проверить что это response
-
-      this.incomeResponsesEvent.removeListener(handlerIndex);
-      clearTimeout(timeout as any);
-
-      promised.resolve(response);
-
-      // if (response.status === ConnectionStatus.responseError) {
-      //   promised.reject(new Error(response.error));
-      // }
-      // else {
-      //   promised.resolve(response);
-      // }
-    });
-
-    timeout = setTimeout(() => {
-      if (promised.isFulfilled()) return;
-
-      this.incomeResponsesEvent.removeListener(handlerIndex);
-
-      promised.reject(new Error(
-        `WsServerConnection.request: Timeout of request has been exceeded ` +
-        `of channel "${channel}"`
-      ));
-    }, this.config.config.requestTimeoutSec * 1000);
-
-    return promised.promise;
+  protected write(sessionId: string, data: Uint8Array): Promise<void> {
+    return this.server.send(sessionId, data);
   }
 
-  onRequest(handler: ConnectionOnRequestHandler): number {
-    const cbWrapper = (
-      request: ConnectionRequest,
-      sessionId: string
-    ) => {
-      try {
-        handler(request, sessionId)
-          .then((response: ConnectionResponse) => this.sendResponseBack(response, sessionId))
-          .catch((e) => {
-            this.sendErrorResponseBack(e, request, sessionId);
-          });
-      }
-      catch (e) {
-        this.sendErrorResponseBack(e, request, sessionId);
-      }
-    };
-
-    return this.incomeRequestsEvent.addListener(cbWrapper);
-  }
-
-  removeListener(handlerIndex: number): void {
-    this.incomeRequestsEvent.removeListener(handlerIndex);
-  }
-
-
-  private async sendResponseBack(response: ConnectionResponse, sessionId: string) {
-    const responseMessage: Uint8Array = encodeResponse(response);
-    // TODO: отправить ответ
-    await this.server.send(sessionId, responseMessage);
-    // TODO: add timeout
-  }
-
-  private sendErrorResponseBack(error: Error, request: ConnectionRequest, sessionId: string) {
-    // TODO: отправить ответ
-    // TODO: add timeout
-
-    // const response: NetworkResponse = {
-    //   requestId: request.requestId,
-    //   status: NetworkStatus.errorMessage,
-    //   body: new Uint8Array(stringToUint8Array(String(e))),
-    // };
-    //
-    // this.sendResponse(port, response)
-    //   .catch(this.log.error);
-  }
-
-  private handleIncomeMessage = (sessionId: string, data: string | Uint8Array) => {
-    if (!(data instanceof Uint8Array) || !data.length) return;
-
-    // TODO: проверить заголовок чтобы определить что это комманда для connection
-
-    const decodedMessage: ConnectionRequest | ConnectionResponse = decodeIncomeMessage(data);
-
-    if (isRequest(decodedMessage)) {
-      // request
-      this.incomeRequestsEvent.emit(decodedMessage as ConnectionRequest, sessionId);
-    }
-    else {
-      // response
-      this.incomeResponsesEvent.emit(decodedMessage as ConnectionResponse, sessionId);
-    }
-  }
 
 }
---
+++ system/lib/base/ConnectionBase.ts
@@ -0,0 +1,141 @@
+import ServiceBase from '../../base/ServiceBase';
+import Connection, {
+  ConnectionOnRequestHandler,
+  ConnectionRequest,
+  ConnectionResponse
+} from '../../interfaces/Connection';
+import IndexedEvents from '../IndexedEvents';
+import {
+  decodeIncomeMessage,
+  encodeRequest,
+  encodeResponse,
+  isRequest,
+  makeConnectionRequest
+} from '../connectionHelpers';
+import Promised from '../Promised';
+
+
+type Timeout = NodeJS.Timeout;
+type IncomeRequestHandler = (request: ConnectionRequest, sessionId: string) => void;
+type IncomeResponseHandler = (response: ConnectionResponse, sessionId: string) => void;
+
+
+export default abstract class ConnectionBase<Props> extends ServiceBase<Props> implements Connection {
+  protected incomeRequestsEvent = new IndexedEvents<IncomeRequestHandler>();
+  protected incomeResponsesEvent = new IndexedEvents<IncomeResponseHandler>();
+
+  protected abstract write(sessionId: string, data: Uint8Array): Promise<void>;
+
+
+  async request(sessionId: string, channel: number, data: Uint8Array): Promise<ConnectionResponse> {
+    const request: ConnectionRequest = makeConnectionRequest(channel, data);
+    const requestMessage: Uint8Array = encodeRequest(request);
+    // send request and wait while sending is finished
+    await this.write(sessionId, requestMessage);
+
+    const promised = new Promised<ConnectionResponse>();
+    let timeout: Timeout | undefined;
+
+    // wait for response
+    const handlerIndex = this.incomeResponsesEvent.addListener((
+      response: ConnectionResponse
+    ) => {
+      // check if promised if fulfilled in case of timeout has exceeded
+      if (promised.isFulfilled()) return;
+      // listen only our response
+      if (response.requestId !== request.requestId) return;
+
+      // TODO: проверить что это response
+
+      this.incomeResponsesEvent.removeListener(handlerIndex);
+      clearTimeout(timeout as any);
+
+      promised.resolve(response);
+
+      // if (response.status === ConnectionStatus.responseError) {
+      //   promised.reject(new Error(response.error));
+      // }
+      // else {
+      //   promised.resolve(response);
+      // }
+    });
+
+    timeout = setTimeout(() => {
+      if (promised.isFulfilled()) return;
+
+      this.incomeResponsesEvent.removeListener(handlerIndex);
+
+      promised.reject(new Error(
+        `WsServerConnection.request: Timeout of request has been exceeded ` +
+        `of channel "${channel}"`
+      ));
+    }, this.config.config.requestTimeoutSec * 1000);
+
+    return promised.promise;
+  }
+
+  onRequest(handler: ConnectionOnRequestHandler): number {
+    const cbWrapper = (
+      request: ConnectionRequest,
+      sessionId: string
+    ) => {
+      try {
+        handler(request, sessionId)
+          .then((response: ConnectionResponse) => this.sendResponseBack(response, sessionId))
+          .catch((e) => {
+            this.sendErrorResponseBack(e, request, sessionId);
+          });
+      }
+      catch (e) {
+        this.sendErrorResponseBack(e, request, sessionId);
+      }
+    };
+
+    return this.incomeRequestsEvent.addListener(cbWrapper);
+  }
+
+  removeListener(handlerIndex: number): void {
+    this.incomeRequestsEvent.removeListener(handlerIndex);
+  }
+
+
+  protected handleIncomeMessage = (sessionId: string, data: Uint8Array) => {
+    if (!(data instanceof Uint8Array) || !data.length) return;
+
+    // TODO: проверить заголовок чтобы определить что это комманда для connection
+
+    const decodedMessage: ConnectionRequest | ConnectionResponse = decodeIncomeMessage(data);
+
+    if (isRequest(decodedMessage)) {
+      // request
+      this.incomeRequestsEvent.emit(decodedMessage as ConnectionRequest, sessionId);
+    }
+    else {
+      // response
+      this.incomeResponsesEvent.emit(decodedMessage as ConnectionResponse, sessionId);
+    }
+  }
+
+
+  protected async sendResponseBack(response: ConnectionResponse, sessionId: string) {
+    const responseMessage: Uint8Array = encodeResponse(response);
+    // TODO: отправить ответ
+    await this.write(sessionId, responseMessage);
+    // TODO: add timeout
+  }
+
+  protected sendErrorResponseBack(error: Error, request: ConnectionRequest, sessionId: string) {
+    // TODO: отправить ответ
+    // TODO: add timeout
+
+    // const response: NetworkResponse = {
+    //   requestId: request.requestId,
+    //   status: NetworkStatus.errorMessage,
+    //   body: new Uint8Array(stringToUint8Array(String(e))),
+    // };
+    //
+    // this.sendResponse(port, response)
+    //   .catch(this.log.error);
+  }
+
+}
