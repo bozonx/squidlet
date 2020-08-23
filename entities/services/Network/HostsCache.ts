648b25e236b8fc0fa05c12e490866ce9d482010a
ivan <ipkozyrin@gmal.com>
2020-08-24 08:18:30 +1000

    add

---
+++ entities/services/Network/HostsCache.ts
@@ -0,0 +1,16 @@
+export interface HostItem {
+  hostId: string;
+  connectionId: string;
+  connectionName: string;
+}
+
+
+/**
+ * Closest hosts cache
+ */
+export default class HostsCache {
+  resolveByHost(hostId: string): HostItem {
+
+  }
+
+}
