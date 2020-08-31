cb9062cf4c2c82cf41aea5801693b327d63f73ca
ivan <ipkozyrin@gmal.com>
2020-09-01 09:56:35 +1000

    change

--- entities/services/Network/RouteResolver.ts
+++ entities/services/Network/RouteResolver.ts
@@ -1,4 +1,11 @@
 
+/*
+* смотрим хост с кэше маршрутов
+* если есть то отправляем по этому маршруту
+* если не удалось или нет кэша маршрута то делаем резолв хоста
+* отправляем широковещательный запрос и собираем обратный путь, 1й вернувшийся ответ записывается в кэш как маршрут, остальные игнорируются
+ */
+
 export default class RouteResolver {
   // host ids like {peerId: hostId}
   private hostIds: {[index: string]: string} = {};
@@ -8,6 +15,7 @@ export default class RouteResolver {
   }
 
   destroy() {
+    delete this.hostIds;
   }
 
 
