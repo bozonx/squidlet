2bf1055037493a5319dc2c6de019c1d51077d4dd
ivan <ipkozyrin@gmal.com>
2020-08-31 17:39:02 +1000

    change

--- system/interfaces/Connection.ts
+++ system/interfaces/Connection.ts
@@ -1,46 +1,10 @@
-/*
- * request
- * * port
- * * requestId
- * * status 255 = request
- * * any data
- *
- * response
- * * port
- * * requestId
- * * status 0 == OK, >0<255 = response error code
- * * any data | no data
- */
-
-// TODO: review
-export enum ConnectionStatus {
-  responseOk = 253,
-  // TODO: может ошибку обрабатывать не на этом уровне а выше?
-  // payload contains an error string
-  responseError = 254,
-}
-
 export interface ConnectionMessage {
   port: number;
-  // should be 16 bits
-  requestId: number;
-}
-
-export interface ConnectionRequest extends ConnectionMessage {
   payload: Uint8Array;
 }
 
-export interface ConnectionResponse extends ConnectionMessage {
-  status: ConnectionStatus;
-  // it will be undefined on error
-  payload?: Uint8Array;
-  error?: string;
-}
-
-export type ConnectionOnRequestHandler = (
-  request: ConnectionRequest,
-  peerId: string
-) => Promise<Uint8Array>;
+export type IncomeDataHandler = (peerId: string, port: number, payload: Uint8Array) => Promise<void>;
+export type PeerStatusHandler = (peerId: string) => Promise<void>;
 export type ConnectionServiceType = 'connection';
 
 // TODO: может где-то сделать enum ???
@@ -51,35 +15,17 @@ export default interface Connection {
   serviceType: ConnectionServiceType;
 
   /**
-   * Send data and waiting of response.
-   * On the other side you should listen to this port and send request.
-   * An error will be risen only if request hasn't been sent or on response timeout.
-   * Port is from 0 to 255 but don't use port 254 and 255.
+   * Send data to peer and don't wait for response.
+   * Port is from 0 and up to 255.
    */
-  request(peerId: string, port: number, payload: Uint8Array): Promise<ConnectionResponse>;
+  send(peerId: string, port: number, payload: Uint8Array): Promise<void>;
+
+  onIncomeData(cb: IncomeDataHandler): number;
+  onPeerConnect(cb: PeerStatusHandler): number;
+  onPeerDisconnect(cb: PeerStatusHandler): number;
 
   /**
-   * Handle income requests at specified port.
-   * Only one handler of specified port is allowed.
-   * You have to generate a response in your handler.
-   * Reserved channels:
-   * * 252 - network service messages
-   * * 253 - response status OK
-   * * 254 - response status Error
-   * * 255 - broadcast message
-   */
-  startListenPort(port: number, handler: ConnectionOnRequestHandler): void;
-
-  /**
-   * Remove listener of onRequest
-   */
-  stopListenPort(port: number): void;
-
-  onPeerConnect(cb: (peerId: string) => void): number;
-  onPeerDisconnect(cb: (peerId: string) => void): number;
-
-  /**
-   * Remove listener of onPeerConnect or onPeerDisconnect
+   * Remove listener of onIncomeData, onPeerConnect or onPeerDisconnect
    */
   removeListener(handlerIndex: number): void;
 }
