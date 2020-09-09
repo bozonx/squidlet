1caafe77bb2b6bca4704dd51f4e891e1e42c84d4
ivan <ipkozyrin@gmal.com>
2020-09-09 20:27:00 +1000

    add

--- entities/services/Transmitter/parseIncomeMessage.ts
+++ entities/services/Transmitter/parseIncomeMessage.ts
@@ -1,12 +1,21 @@
 
-export enum RESULT_POSITIONS {
+export enum MESSAGE_POSITIONS {
   functionNum,
   functionsArgs,
 }
 
-function parseDigitalOutput(data: Uint8Array): [number, any[]] {
-  // TODO: add
+function parseDigitalOutput(data: Uint8Array): [number, boolean] {
+  if (data.length !== 2) {
+    throw new Error(`Function parseDigitalOutput: Invalid length of data passed`);
+  }
+  // TODO: может делать структурой ???
+  // pin number, currentState
+  return [data[0], !!data[1]];
 }
+
+const parseFunctions: {[index: string]: (data: Uint8Array) => any[]} = {
+  12: parseDigitalOutput,
+};
 
 
 export default function parseIncomeMessage(
@@ -15,7 +24,18 @@ export default function parseIncomeMessage(
   const result: [number, any[]][] = [];
 
   for (let item of messages) {
-    // TODO: add
+    const functionNum: number = item[MESSAGE_POSITIONS.functionNum];
+    // TODO: test
+    const data: Uint8Array = item.slice(MESSAGE_POSITIONS.functionsArgs);
+
+    if (!parseFunctions[functionNum]) {
+      // TODO: better to do log.warn and just skip
+      throw new Error(`Can't recognize the function handler: ${functionNum}`);
+    }
+
+    const args: any[] = parseFunctions[functionNum](data);
+
+    result.push([functionNum, args]);
   }
 
   return result;
