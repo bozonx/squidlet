82bf9acc8f3cdd5390e91aca48596e8d978f6a94
ivan <ipkozyrin@gmal.com>
2020-09-09 20:31:21 +1000

    add

--- entities/services/Transmitter/parseIncomeMessage.ts
+++ entities/services/Transmitter/parseIncomeMessage.ts
@@ -1,27 +1,43 @@
+export interface DigitalOutputResult {
+  pin: number;
+  state: boolean;
+}
+
+// TODO: review
+export interface I2cResult {
+  data: Uint8Array;
+}
+
+
+export type Results = DigitalOutputResult | I2cResult;
 
 export enum MESSAGE_POSITIONS {
   functionNum,
   functionsArgs,
 }
 
-function parseDigitalOutput(data: Uint8Array): [number, boolean] {
+function parseDigitalOutput(data: Uint8Array): DigitalOutputResult {
   if (data.length !== 2) {
     throw new Error(`Function parseDigitalOutput: Invalid length of data passed`);
   }
   // TODO: может делать структурой ???
   // pin number, currentState
-  return [data[0], !!data[1]];
+  return {
+    pin: data[0],
+    state: Boolean(data[1]),
+  };
 }
 
-const parseFunctions: {[index: string]: (data: Uint8Array) => any[]} = {
+
+const parseFunctions: {[index: string]: (data: Uint8Array) => Results} = {
   12: parseDigitalOutput,
 };
 
 
 export default function parseIncomeMessage(
   messages: Uint8Array[]
-): [number, any[]][] {
-  const result: [number, any[]][] = [];
+): [number, Results][] {
+  const result: [number, Results][] = [];
 
   for (let item of messages) {
     const functionNum: number = item[MESSAGE_POSITIONS.functionNum];
@@ -33,7 +49,7 @@ export default function parseIncomeMessage(
       throw new Error(`Can't recognize the function handler: ${functionNum}`);
     }
 
-    const args: any[] = parseFunctions[functionNum](data);
+    const args: Results = parseFunctions[functionNum](data);
 
     result.push([functionNum, args]);
   }
