eebe4a5c0a1f72bea7d3e8f70f03c1e0879abcca
ivan <ipkozyrin@gmal.com>
2020-09-09 10:15:08 +1000

    add

--- testModbus3.ts
+++ testModbus3.ts
@@ -5,7 +5,7 @@ const Modbus = require('jsmodbus');
 const SerialPort = require('serialport');
 const options = {
   baudRate: 9600,
-  parity: 'even',
+  parity: 'none',
   dataBits: 8,
   stopBits: 1,
 };
@@ -52,30 +52,23 @@ function makePinWritePackage(pinNum: number, state: boolean): number[] {
 
 
 
-// const oldWrite = socket.write.bind(socket);
-// socket.write = (
-//   data: string| number[] | Buffer,
-//   // TODO: use encoding ???
-//   callback?: (error: Error | null | undefined, bytesWritten: number) => void
-// ): boolean => {
-//
-//   const cbWrapper = (error: Error | null | undefined, bytesWritten: number): void => {
-//     gpiop.write(TRANSMITTER_PIN, TRANSMITTER_RECEIVE_STATE);
-//
-//     if (callback) callback(error, bytesWritten);
-//   };
-//
-//   gpiop.write(TRANSMITTER_PIN, TRANSMITTER_SEND_STATE)
-//     .then(() => {
-//       const result = oldWrite(data, cbWrapper);
-//
-//       console.log(22222222, result);
-//     })
-//     .catch(console.error);
-//
-//   // TODO: поидее то неправлиьно, состояние то неизвестно
-//   return true;
-// };
+const oldWrite = socket.write.bind(socket);
+socket.write = (
+  data: string| number[] | Buffer,
+  // TODO: use encoding ???
+  callback?: (error: Error | null | undefined, bytesWritten: number) => void
+): boolean => {
+
+  console.log('write', data);
+
+  const cbWrapper = (error: Error | null | undefined, bytesWritten: number): void => {
+    console.log('wrappercb', error, bytesWritten);
+
+    if (callback) callback(error, bytesWritten);
+  };
+
+  return oldWrite(data, cbWrapper);
+};
 
 
 
@@ -90,27 +83,23 @@ socket.on('open', function () {
   const pinSetupMessage: number[] = makePinSetupPackage(pinNumber);
   const pinWriteMessage: number[] = makePinWritePackage(pinNumber, pinState);
 
-  client.writeMultipleRegisters(0x00, pinSetupMessage)
-    //client.writeMultipleRegisters(0x00, [65110, 65120, 65130, 65140])
-    //client.writeMultipleRegisters(0x06, [0x00, 0x01, 0x03, 0x00])
-    .then((data: any) => console.log('writing result ', data))
-    .catch(handleErrors);
+  // client.writeMultipleRegisters(0x00, pinSetupMessage)
+  //   .then((data: any) => console.log('writing result ', data))
+  //   .catch(handleErrors);
+  //
+  // setTimeout(() => {
+  //   client.writeMultipleRegisters(0x00, pinWriteMessage)
+  //     .then((data: any) => console.log('writing result ', data))
+  //     .catch(handleErrors);
+  // }, 100);
 
   setTimeout(() => {
-    client.writeMultipleRegisters(0x00, pinWriteMessage)
-      //client.writeMultipleRegisters(0x00, [65110, 65120, 65130, 65140])
-      //client.writeMultipleRegisters(0x06, [0x00, 0x01, 0x03, 0x00])
-      .then((data: any) => console.log('writing result ', data))
-      .catch(handleErrors);
-  }, 500);
-
-  // setTimeout(() => {
-  //   //client.readHoldingRegisters(0x05, 1)
-  //   client.readInputRegisters(0x03, 1)
-  //     .then((data) => console.log('reading result ', data))
-  //     .catch(handleErrors)
-  //     .finally(() => socket.close());
-  // }, 200)
+    client.readHoldingRegisters(1, 2)
+    //client.readInputRegisters(0, 3)
+      .then((data: any) => console.log('reading result ', data))
+      .catch(handleErrors)
+      .finally(() => socket.close());
+  }, 200);
 
 });
 
