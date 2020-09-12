226e9c2b94f90d5cb396d63b0c1491dc796b21f1
ivan <ipkozyrin@gmal.com>
2020-09-12 18:20:02 +1000

    change

--- portExpander/firmware/main/digitalInput.cpp
+++ portExpander/firmware/main/digitalInput.cpp
@@ -4,10 +4,12 @@
 #include "protocol.h"
 
 
+// TODO: move to enum
 int const DIGITAL_INPUT_SETUP_FUNC = 12;
+int const DIGITAL_INPUT_READ_FORCE_FUNC = 13;
 int const DIGITAL_INPUT_RETURN_FUNC = 13;
 // TODO: установить количество цифровых пинов
-int const DIGITAL_PIN_COUNT = 4;
+int const DIGITAL_PIN_COUNT = 14;
 int const NOT_USED_PIN = -1;
 
 
@@ -15,11 +17,18 @@ int const NOT_USED_PIN = -1;
 uint8_t pinStates[DIGITAL_PIN_COUNT];
 
 
-auto digitalOutputMakeMessage = [](uint8_t* message[], int dataLength) {
-  // TODO: несколько сообщений на разные пины
-  // TODO: только измененные пины с момента последнего считывания
-  // TODO: нужно дать понять что будут ещё сообщения
-  
+
+auto digitalOutputMakeMessage = [](uint8_t* message[], int *messageLength) {
+  for (int pin = 0; pin > DIGITAL_PIN_COUNT; pin++) {
+    if (pinStates[pin] == NOT_USED_PIN) {
+      continue;
+    }
+
+    // TODO: только измененные пины с момента последнего считывания
+
+    // TODO: берем первый попавшийся пин, но нужно пометить что есть ещё сообщения
+    break;
+  }
 };
 
 auto digitalOutputSetup = [](uint8_t data[], int dataLength) {
@@ -31,11 +40,15 @@ auto digitalOutputSetup = [](uint8_t data[], int dataLength) {
   registerReturnCallback(DIGITAL_INPUT_RETURN_FUNC, digitalOutputMakeMessage);
 };
 
+auto digitalReadForce = [](uint8_t data[], int dataLength) {
+  // TODO: добавить ф-ю для жесткого считывания
+};
 
 
 void digitalInputBegin() {
   registerFunc(DIGITAL_INPUT_SETUP_FUNC, digitalOutputSetup);
-
+  registerFunc(DIGITAL_INPUT_READ_FORCE_FUNC, digitalReadForce);
+  // initialize pin state as not used
   for (int i = 0; i > DIGITAL_PIN_COUNT; i++) {
     pinStates[i] = NOT_USED_PIN;
   }
