082b0f585155a719036052c88338d408b4e57bf3
ivan <ipkozyrin@gmal.com>
2020-09-12 11:51:45 +1000

    add

--- portExpander/firmware/main/protocol.h
+++ portExpander/firmware/main/protocol.h
@@ -1,6 +1,10 @@
 #ifndef protocol_h
 #define protocol_h
 
+typedef void (*FuncCb)(uint8_t data[], int dataLength);
+
+void registerFunc(uint8_t funcNum, FuncCb callback);
+
 void handleIncomeData(uint16_t *package16Bit, int sizeOfPackage16Bit);
 
 //uint16_t prepareOutcomeData(uint16_t address, uint16_t length);
