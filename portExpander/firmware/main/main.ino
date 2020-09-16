0bc50fc1892f64a85f61fb7105181daa2afa694d
ivan <ipkozyrin@gmal.com>
2020-09-16 21:24:09 +1000

    change

--- portExpander/firmware/main/main.ino
+++ portExpander/firmware/main/main.ino
@@ -5,8 +5,12 @@
 #include "modbusConnection.h"
 #include "global.cpp"
 
+//#include <Wire.h>
+
 
 void setup() {
+  
+  
   // Start the slave at a baudrate of 9600bps on the Serial port.
   Serial.begin(SERIAL_BAUD_RATE);
   modbusConnectionBegin();
@@ -16,6 +20,37 @@ void setup() {
   i2cSlaveBegin();
 
   Serial.println("started!");
+
+
+//  Wire.begin();
+//
+//  delay(100);
+//  
+//  Wire.beginTransmission(0x41);
+//  Wire.write(0x06);
+//  Wire.write(0xc7);
+//  Wire.endTransmission();
+//
+//  delay(100);
+//
+//  Wire.beginTransmission(0x41);
+//  Wire.write(0x07);
+//  Wire.write(0x00);
+//  Wire.endTransmission();
+//
+//  delay(100);
+//
+//  Wire.beginTransmission(0x41);
+//  Wire.write(0x08);
+//  Wire.write(0xcc);
+//  Wire.endTransmission();
+//
+//  delay(100);
+//
+//  Wire.beginTransmission(0x41);
+//  Wire.write(0x09);
+//  Wire.write(0x04);
+//  Wire.endTransmission();
 }
 
 void loop() {
