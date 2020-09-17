6318698c59c9d67a485a40b9a79308d1cdb61bac
ivan <ipkozyrin@gmal.com>
2020-09-17 18:37:17 +1000

    refactor

--- entities/systemEntitiesPlugin.ts
+++ entities/systemEntitiesPlugin.ts
@@ -46,7 +46,7 @@ export default async function systemEntitiesPlugin (env: PluginEnv) {
   await env.addService(path.join(servicesRoot, 'Automation/manifest.yaml'));
   await env.addService(path.join(servicesRoot, 'HttpApi/manifest.yaml'));
   await env.addService(path.join(servicesRoot, 'IoServer/manifest.yaml'));
-  await env.addService(path.join(servicesRoot, 'IoSetPcf8574/manifest.yaml'));
+  await env.addService(path.join(servicesRoot, 'Pcf8574/manifest.yaml'));
   await env.addService(path.join(servicesRoot, 'ModbusMasterConnection/manifest.yaml'));
   //await env.addService(path.join(servicesRoot, 'Network/manifest.yaml'));
   await env.addService(path.join(servicesRoot, 'MqttApiTopics/manifest.yaml'));
