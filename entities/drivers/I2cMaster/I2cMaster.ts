7c3ba7a6a8bb6eda1c18a04582fdb403cabe95b0
ivan <ipkozyrin@gmal.com>
2020-09-17 18:12:58 +1000

    change

--- entities/drivers/I2cMaster/I2cMaster.ts
+++ entities/drivers/I2cMaster/I2cMaster.ts
@@ -1,39 +1,32 @@
 import DriverFactoryBase from 'system/base/DriverFactoryBase';
 import {hexNumToString, hexStringToHexNum} from 'system/lib/binaryHelpers';
-import MasterSlaveBaseNodeDriver, {MasterSlaveBaseProps} from 'system/lib/base/MasterSlaveBaseNodeDriver';
 import I2cMasterIo from 'system/interfaces/io/I2cMasterIo';
-import {ImpulseInput} from '../ImpulseInput/ImpulseInput';
+import DriverBase from 'system/base/DriverBase';
 
 
-export interface I2cMasterDriverProps extends MasterSlaveBaseProps {
+export interface I2cMasterDriverProps {
   busNum: number;
   // it can be i2c address as a string like '0x5a' or number equivalent - 90
   address: string | number;
 }
 
 
-export class I2cMaster extends MasterSlaveBaseNodeDriver<I2cMasterDriverProps> {
-  private impulseInput?: ImpulseInput;
-  private impulseHandlerIndex?: number;
+export class I2cMaster extends DriverBase<I2cMasterDriverProps> {
+  //protected sender!: Sender;
   // converted address string or number to hex. E.g '5a' => 90, 22 => 34
-  private addressHex: number = -1;
-
-  private get i2cMasterIo(): I2cMasterIo {
-    return this.depsInstances.i2cMaster;
-  }
+  private addressHex!: number;
+  private i2cMasterIo!: I2cMasterIo;
 
 
   init = async () => {
-    super.init();
+    this.i2cMasterIo = this.context.getIo('I2cMaster');
 
-    if (this.props.int) {
-      this.impulseInput = await this.context.getSubDriver<ImpulseInput>(
-        'ImpulseInput',
-        this.props.int || {}
-      );
-    }
-
-    this.depsInstances.i2cMaster = this.context.getIo('I2cMaster');
+    // this.sender = new Sender(
+    //   this.context.config.config.requestTimeoutSec,
+    //   this.context.config.config.senderResendTimeout,
+    //   this.context.log.debug,
+    //   this.context.log.warn
+    // );
 
     if (typeof this.props.address === 'string') {
       this.addressHex = hexStringToHexNum(String(this.props.address));
@@ -50,21 +43,29 @@ export class I2cMaster extends MasterSlaveBaseNodeDriver<I2cMasterDriverProps> {
       `addr: ${hexNumToString(this.addressHex)}, data: ${JSON.stringify(data)}`
     );
 
-    return this.sender.send<void>(
-      undefined,
-      (): Promise<void> => {
-        return this.i2cMasterIo.i2cWriteDevice(this.props.busNum, this.addressHex, data);
-      }
-    );
+    return this.i2cMasterIo.i2cWriteDevice(this.props.busNum, this.addressHex, data);
+
+    // return this.sender.send<void>(
+    //   undefined,
+    //   (): Promise<void> => {
+    //     return this.i2cMasterIo.i2cWriteDevice(this.props.busNum, this.addressHex, data);
+    //   }
+    // );
   }
 
   async read(length: number): Promise<Uint8Array> {
-    const result: Uint8Array = await this.sender.send<Uint8Array>(
-      undefined,
-      (): Promise<Uint8Array> => {
-        return this.i2cMasterIo.i2cReadDevice(this.props.busNum, this.addressHex, length);
-      }
+    const result: Uint8Array = await this.i2cMasterIo.i2cReadDevice(
+      this.props.busNum,
+      this.addressHex,
+      length
     );
+
+    // const result: Uint8Array = await this.sender.send<Uint8Array>(
+    //   undefined,
+    //   (): Promise<Uint8Array> => {
+    //     return this.i2cMasterIo.i2cReadDevice(this.props.busNum, this.addressHex, length);
+    //   }
+    // );
 
     this.log.debug(
       `I2cMaster driver read. busNum ${this.props.busNum}, ` +
@@ -72,34 +73,6 @@ export class I2cMaster extends MasterSlaveBaseNodeDriver<I2cMasterDriverProps> {
     );
 
     return result;
-  }
-
-  startFeedback(): void {
-    if (this.props.feedback === 'int') {
-      if (!this.impulseInput) {
-        throw new Error(
-          `MasterSlaveBaseNodeDriver.startFeedback. impulseInput driver hasn't been set. ${JSON.stringify(this.props)}`
-        );
-      }
-
-      this.impulseHandlerIndex = this.impulseInput.onChange(this.pollAllFunctions);
-
-      return;
-    }
-
-    super.startFeedback();
-  }
-
-  stopFeedBack() {
-    if (this.props.feedback === 'int') {
-      if (!this.impulseHandlerIndex) return;
-
-      this.impulseInput && this.impulseInput.removeListener(this.impulseHandlerIndex);
-
-      return;
-    }
-
-    super.stopFeedBack();
   }
 
   // private makeSenderId(functionHex: number | undefined, method: string, ...params: (string | number)[]) {
