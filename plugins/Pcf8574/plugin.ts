77213f6a8879a2478b2f4ee1853c3423a7c5eec4
ivan <ipkozyrin@gmal.com>
2020-09-17 18:42:04 +1000

    add

---
+++ plugins/Pcf8574/plugin.ts
@@ -0,0 +1,7 @@
+import * as path from 'path';
+import PluginEnv from '../../hostEnvBuilder/entities/PluginEnv';
+
+
+export default async function systemEntitiesPlugin (env: PluginEnv) {
+  await env.addService(path.resolve(__dirname, './services/IoSetPcf8574/manifest.yaml'));
+}
