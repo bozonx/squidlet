e56635594a577f08a5e3eb64a1c3e0816ff31a0f
ivan <ipkozyrin@gmal.com>
2020-09-18 11:42:56 +1000

    refactor

--- plugins/Pcf8574/plugin.ts
+++ plugins/Pcf8574/plugin.ts
@@ -3,5 +3,6 @@ import PluginEnv from '../../hostEnvBuilder/entities/PluginEnv';
 
 
 export default async function systemEntitiesPlugin (env: PluginEnv) {
+  await env.addDriver(path.resolve(__dirname, './drivers/Pcf8574/manifest.yaml'));
   await env.addService(path.resolve(__dirname, './services/IoSetPcf8574/manifest.yaml'));
 }
