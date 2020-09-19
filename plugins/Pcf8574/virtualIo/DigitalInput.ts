125dbcf9b191f3f3ce1ffd584d5c9770d843c7d9
ivan <ipkozyrin@gmal.com>
2020-09-19 14:37:20 +1000

    change

--- plugins/Pcf8574/virtualIo/DigitalInput.ts
+++ plugins/Pcf8574/virtualIo/DigitalInput.ts
@@ -1,44 +1,5 @@
-import DigitalInputIo, {ChangeHandler} from 'system/interfaces/io/DigitalInputIo';
-import {Edge, InputResistorMode} from 'system/interfaces/gpioTypes';
+import DigitalExpanderInputLogic from 'system/logic/DigitalExpander/DigitalExpanderInputLogic';
 
 
-export default class DigitalInput implements DigitalInputIo {
-  constructor() {
-  }
-
-
-  setup(
-    pin: number,
-    inputMode: InputResistorMode,
-    debounce: number,
-    edge: Edge
-  ): Promise<void> {
-
-  }
-
-  getPinResistorMode(pin: number): Promise<InputResistorMode | undefined> {
-
-  }
-
-  read(pin: number): Promise<boolean> {
-
-  }
-
-  onChange(pin: number, handler: ChangeHandler): Promise<number> {
-
-  }
-
-  removeListener(handlerIndex: number): Promise<void> {
-
-  }
-
-  clearPin(pin: number): Promise<void> {
-
-  }
-
-  clearAll(): Promise<void> {
-    for (let pin of Object.keys(this.pinParams)) {
-      await this.clearPin(parseInt(pin));
-    }
-  }
+export default class DigitalInput extends DigitalExpanderInputLogic {
 }
