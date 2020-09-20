39bf70fc9b218436324868765219e41f2bfe064d
ivan <ipkozyrin@gmal.com>
2020-09-20 10:34:25 +1000

    refactor

--- system/logic/digitalExpander/DigitalExpanderOutputLogic.ts
+++ system/logic/digitalExpander/DigitalExpanderOutputLogic.ts
@@ -1,31 +1,62 @@
 import {OutputResistorMode} from '../../interfaces/gpioTypes';
 import DigitalOutputIo from '../../interfaces/io/DigitalOutputIo';
 import DigitalExpanderLogic from './DigitalExpanderLogic';
+import QueueOverride from '../../lib/QueueOverride';
+import DebounceCall from '../../lib/debounceCall/DebounceCall';
+import Context from '../../Context';
 
 
 export default class DigitalExpanderOutputLogic implements DigitalOutputIo {
-  private logic: DigitalExpanderLogic;
+  private readonly context: Context;
+  private readonly logic: DigitalExpanderLogic;
+  private readonly writeBufferMs?: number;
+  private readonly queue: QueueOverride;
+  private readonly debounce = new DebounceCall();
+  // temporary state while values are buffering before writing
+  private beforeWritingBuffer?: number;
+  // temporary state while writing
+  private writingTimeBuffer?: number;
 
 
-  // TODO: принимать logError ???
-  constructor(logic: DigitalExpanderLogic) {
+  // TODO: использовать props ???
+  constructor(context: Context, logic: DigitalExpanderLogic, writeBufferMs?: number) {
+    this.context = context;
     this.logic = logic;
+    this.writeBufferMs = writeBufferMs;
+    this.queue = new QueueOverride(context.config.config.queueJobTimeoutSec);
+  }
+
+  destroy = async () => {
+    this.queue.destroy();
+    this.debounce.destroy();
   }
 
 
   setup(pin: number, initialValue: boolean, outputMode: OutputResistorMode): Promise<void> {
-
+    return this.logic.setupOutput(pin, initialValue, outputMode);
   }
 
-  getPinResistorMode(pin: number): Promise<OutputResistorMode | undefined> {
-
+  async getPinResistorMode(pin: number): Promise<OutputResistorMode | undefined> {
+    return this.logic.getPinResistorMode(pin) as OutputResistorMode | undefined;
   }
 
   // // output and input pins can be read
   // read(pin: number): Promise<boolean>;
 
   write(pin: number, value: boolean): Promise<void> {
+    // in case it is writing at the moment - save buffer and add cb to queue
+    if (this.isWriting()) {
+      return this.invokeAtWritingTime(pin, value);
+    }
+    // start buffering step or update buffer
+    else if(this.writeBufferMs) {
+      // in buffering case collect data at the buffering time (before writing)
+      return this.invokeBuffering(pin, value);
+    }
+    // else if buffering doesn't set - just start writing
+    const stateToWrite = updateBitInByte(this.getState(), pin, value);
 
+    return this.startWriting(stateToWrite);
   }
 
   clearPin(pin: number): Promise<void> {
--- system/logic/digitalExpander/interfaces/DigitalExpanderDriver.ts
+++ system/logic/digitalExpander/interfaces/DigitalExpanderDriver.ts
@@ -1,5 +1,36 @@
-export default interface DigitalExpanderDriver {
-  readState(): Promise<Uint8Array>;
+import {Edge, PinDirection} from '../../../interfaces/gpioTypes';
 
-  writeState(state: Uint8Array): Promise<void>;
+
+export type DigitalExpanderDriverHandler = (newState: {[index: string]: boolean}) => void;
+
+export interface DigitalExpanderPinsProps {
+  direction: PinDirection;
+  // for output pins
+  initialValue: boolean;
+  // for input pin
+  debounce: number;
+  // for input pin
+  edge: Edge;
+}
+
+
+export default interface DigitalExpanderDriver {
+  /**
+   * Setup one or more pins. It can be called several times.
+   */
+  setup(pinsProps: {[index: string]: DigitalExpanderPinsProps}): Promise<void>;
+
+  /**
+   * Write output pins state
+   */
+  writeState(state: {[index: string]: boolean}): Promise<void>;
+
+  /**
+   * Read input pins state
+   */
+  doPoll(): Promise<void>;
+
+  onChange(cb: DigitalExpanderDriverHandler): number;
+
+  removeListener(handlerIndex: number): void;
 }
