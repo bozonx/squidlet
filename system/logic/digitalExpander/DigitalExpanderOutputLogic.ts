bab44d3b2963c2db015ca7bb5546dacf2bf953b8
ivan <ipkozyrin@gmal.com>
2020-10-03 11:49:42 +1000

    add BufferedQueue

--- system/logic/digitalExpander/DigitalExpanderOutputLogic.ts
+++ system/logic/digitalExpander/DigitalExpanderOutputLogic.ts
@@ -1,6 +1,5 @@
-import QueueOverride from '../../lib/QueueOverride';
 import BufferedRequest from '../../lib/BufferedRequest';
-import {isEmptyObject} from '../../lib/objects';
+import BufferedQueue from '../../lib/BufferedQueue';
 
 
 /**
@@ -11,13 +10,7 @@ export default class DigitalExpanderOutputLogic {
   private readonly writeCb: (changedState: {[index: string]: boolean}) => Promise<void>;
   private readonly writeBufferMs?: number;
   private readonly bufferedRequest: BufferedRequest;
-  private readonly queue: QueueOverride;
-  // temporary state while writing
-  private writingState?: {[index: string]: boolean};
-  // temporary state which will be written when queued cb is called
-  private writingQueueBuffer?: {[index: string]: boolean};
-  // The last actual state which is saved to IC
-  private savedState: {[index: string]: boolean} = {};
+  private readonly bufferedQueue: BufferedQueue;
 
 
   constructor(
@@ -30,12 +23,12 @@ export default class DigitalExpanderOutputLogic {
     this.writeCb = writeCb;
     this.writeBufferMs = writeBufferMs;
     this.bufferedRequest = new BufferedRequest(this.startWriting, writeBufferMs);
-    this.queue = new QueueOverride(queueJobTimeoutSec);
+    this.bufferedQueue = new BufferedQueue(queueJobTimeoutSec);
   }
 
   destroy() {
-    this.queue.destroy();
     this.bufferedRequest.destroy();
+    this.bufferedQueue.destroy();
   }
 
 
@@ -45,15 +38,13 @@ export default class DigitalExpanderOutputLogic {
    */
   getState(): {[index: string]: boolean} {
     return {
-      ...this.savedState,
-      // TODO: а то что в буфере ???
-      ...this.writingState || {},
-      ...this.writingQueueBuffer || {},
+      ...this.bufferedQueue.getState(),
+      ...this.bufferedRequest.getState(),
     };
   }
 
   getSavedState(): {[index: string]: boolean} {
-    return this.savedState;
+    return this.bufferedQueue.getSavedState();
   }
 
   isInProgress(): boolean {
@@ -65,22 +56,16 @@ export default class DigitalExpanderOutputLogic {
   }
 
   isWriting(): boolean {
-    return this.queue.isPending();
+    return this.bufferedQueue.isPending();
   }
 
   cancel() {
     this.bufferedRequest.cancel();
-    this.queue.stop();
-
-    delete this.writingState;
-    delete this.writingQueueBuffer;
+    this.bufferedQueue.stop();
   }
 
   clearPin(pin: number) {
-    delete this.savedState[pin];
-
-    if (this.writingState) delete this.writingState[pin];
-    if (this.writingQueueBuffer) delete this.writingQueueBuffer[pin];
+    this.bufferedQueue.clearItem(pin);
   }
 
   write(pin: number, value: boolean): Promise<void> {
@@ -91,16 +76,10 @@ export default class DigitalExpanderOutputLogic {
    * Write full or partial state
    */
   async writeState(partialState: {[index: string]: boolean}): Promise<void> {
-    // If there is some cb waiting - just update buffer
-    // Or if there is writing one job but no queue
-    if (this.queue.isPending() || this.queue.hasQueue()) {
-      // make or update queue buffer
-      this.writingQueueBuffer = {
-        ...this.writingQueueBuffer,
-        ...partialState,
-      };
-      // add job to queue
-      return this.queue.add(this.doWriteCb);
+    // If there is some cb writing or there is a queue then update buffer
+    if (this.bufferedQueue.hasQueue()) {
+      // add to queue or update queued cb and buffer
+      return this.bufferedQueue.add(this.writeCb, partialState);
     }
     // else it is in a buffering state or it is a new request
     return this.bufferedRequest.write(partialState);
@@ -110,58 +89,10 @@ export default class DigitalExpanderOutputLogic {
   /**
    * Start a new writing. It is called after buffering time.
    */
-  private startWriting = (stateToSave: {[index: string]: boolean}): Promise<void> => {
-    // at buffering time writingState is undefined
-    if (this.writingState) throw new Error(`writingState exists at start new writing time`);
-    // use it to pass data to doWriteCb() and get this state in getState()
-    this.writingState = {...stateToSave};
+  private startWriting = (stateToSave: {[index: string]: boolean}) => {
     // first write request will be called write now at the current tick
-    return this.queue.add(this.doWriteCb);
-  }
-
-  private doWriteCb = async () => {
-    if (!this.writingState && !this.writingQueueBuffer) {
-      throw new Error(`no state to write`);
-    }
-
-    // if is writing queued job
-    if (this.writingQueueBuffer) {
-      if (isEmptyObject(this.writingQueueBuffer)) return;
-
-      this.writingState = {...this.writingQueueBuffer};
-
-      this.writingState = this.writingQueueBuffer;
-
-      delete this.writingQueueBuffer;
-    }
-    // else first time - use writingState
-    // Let writingState exist while the writing process is finished because
-    // it will be used in getState()
-
-    if (!this.writingState || isEmptyObject(this.writingState)) return;
-
-    try {
-      await this.writeCb(this.writingState);
-    }
-    catch (e) {
-      // remove all buffers because the queue will be cancelled
-      delete this.writingState;
-      delete this.writingQueueBuffer;
-
-      throw e;
-    }
-
-    // TODO: надо поднять событие что было изменение стейта
-    //       инача переключатели в UI не отработают
-    //       либо это делать уровнем выше на ошибку
-
-    // set a new state which has been just saved
-    this.savedState = {
-      ...this.savedState,
-      ...this.writingState,
-    };
-
-    delete this.writingState;
+    this.bufferedQueue.add(this.writeCb, stateToSave)
+      .catch(this.logError);
   }
 
 }
