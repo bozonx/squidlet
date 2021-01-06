78349605422036ee8846fc77880fb0a54e17f5da
Ivan <ipkozyrin@gmail.com>
2021-01-06 12:26:06 +1000

    change

--- src/plugins/networking/interfaces/BridgeDriver.ts
+++ src/plugins/networking/interfaces/BridgeDriver.ts
@@ -1,3 +1,23 @@
+export interface ConnectionMessage {
+  port: number;
+  payload: Uint8Array;
+}
+
+export interface ConnectionProps {
+  address: number;
+}
+
+
+export type IncomeMessageHandler = (channel: number, payload: Uint8Array) => void;
+export type ConnectionServiceType = 'connection';
+
+export enum ConnectionsEvents {
+  message,
+  connected,
+  disconnected
+}
+
+
 export enum NetworkStatus {
   ok = 0,
   // body contains an error string
@@ -27,7 +47,11 @@ export type IncomeRequestHandler = (request: NetworkRequest) => Promise<NetworkR
 export type IncomeResponseHandler = (request: NetworkResponse) => void;
 
 
-export default interface NetworkDriver {
+export interface BridgeDriver {
+  /**
+   * Send data to peer and don't wait for response.
+   * Channel is from 0 and up to 253. Don't use 254 and 255.
+   */
   /**
    * Send data and waiting of response.
    * On the other side you should listen to this address and send data to the same address on this side.
@@ -35,13 +59,17 @@ export default interface NetworkDriver {
    * Register is 8 bits.
    * Port is from 0 to 255 but don't use port 255 it is registered for network data transfer.
    */
-  request(port: number, body: Uint8Array): Promise<NetworkResponse>;
+  request(channel: number, body: Uint8Array): Promise<NetworkResponse>;
+  isConnected(): boolean;
 
+  // onIncomeMessage(cb: IncomeMessageHandler): number;
   /**
    * Handle income request at specified register.
    * You have to generate a response
    */
   onRequest(port: number, handler: IncomeRequestHandler): number;
+  onConnect(cb: () => void): number;
+  onDisconnect(cb: () => void): number;
 
   /**
    * Remove listener that has been set by `onRequest` or `onResponse`
