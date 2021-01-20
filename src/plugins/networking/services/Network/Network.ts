650b896bf86b3351d761131a7852ec8d909dc86a
Ivan <ipkozyrin@gmail.com>
2021-01-20 22:57:54 +1000

    change

--- src/plugins/networking/services/Network/Network.ts
+++ src/plugins/networking/services/Network/Network.ts
@@ -1,4 +1,5 @@
 import EntityBase from '../../../../base/EntityBase'
+import IndexedEvents from '../../../../../../squidlet-lib/src/IndexedEvents'
 
 
 export type NetworkIncomeRequestHandler = (
@@ -10,31 +11,77 @@ export type NetworkIncomeRequestHandler = (
 
 
 export function encodeNetworkPayload(
-  host: string,
+  toHostId: string,
+  fromHostId: string,
   uri: string,
   payload: Uint8Array
 ): Uint8Array {
+  // TODO: add TTL
+}
+
+export function decodeNetworkMessage(
+  data: Uint8Array
+): [string, string, string, Uint8Array] {
+  // TODO: validate message
 
 }
+
+export function extractToHostIdFromPayload(data: Uint8Array): string {
+
+}
+
 
 export default class Network extends EntityBase {
   private bridgesManager: BridgesManager
   private hostResolver: HostResolver
+  private incomeMessagesEvent = new IndexedEvents<NetworkIncomeRequestHandler>()
+
+
+  async init() {
+    this.bridgesManager.onIncomeMessage(this.handleIncomeMessage)
+  }
 
 
   async send(host: string, uri: string, payload: Uint8Array): Promise<void> {
     const connectionId: string = this.hostResolver.resoveConnection(host)
-    const completePayload = encodeNetworkPayload(host, uri, payload)
+    const completePayload = encodeNetworkPayload(host, this.config.hostId, uri, payload)
 
     await this.bridgesManager.send(connectionId, completePayload)
   }
 
   onIncomeMessage(cb: NetworkIncomeRequestHandler): number {
-    // TODO: add
+    return this.incomeMessagesEvent.addListener(cb)
   }
 
   off(handlerIndex: number) {
-    // TODO: add
+    this.incomeMessagesEvent.removeListener(handlerIndex)
+  }
+
+
+  private handleIncomeMessage = (completePayload: Uint8Array, connectionId: string) => {
+    // TODO: validate message
+
+    const toHostId: string = extractToHostIdFromPayload(completePayload)
+
+    if (this.config.hostId === toHostId) {
+      const [, fromHostId, uri, payload] = decodeNetworkMessage(completePayload)
+
+      this.incomeMessagesEvent.emit(uri, payload, fromHostId)
+    }
+    else {
+      // pass the message further
+      const connectionId: string = this.hostResolver.resoveConnection(toHostId)
+
+      // TODO: уменьшить TTL
+      // TODO: если TTL уже 0 то поднять ошибку
+
+      this.bridgesManager.send(connectionId, completePayload)
+        .catch((e: Error) => {
+          // TODO: вместо записи в лог надо отправить назад ошибку,
+          //       но как-то связать ее с этим сообщением
+          this.log.error(e)
+        })
+    }
   }
 
 }
