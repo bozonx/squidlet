11c1acac1b81a21cb86c2a629b190a70e15b2bb8
Ivan <ipkozyrin@gmail.com>
2023-03-26 11:02:48 +0300

    add

--- __needed/system/interfaces/io/HttpClientIo.ts
+++ __needed/system/interfaces/io/HttpClientIo.ts
@@ -1,5 +1,5 @@
-import {HttpRequest, HttpResponse} from '../../../../__idea2021/networking/interfaces/__old/Http.js';
-import IoItem from '../../../../squidlet/__old/system/interfaces/IoItem';
+import {HttpRequest, HttpResponse} from '../../../../../../../../../mnt/disk2/workspace/squidlet/__idea2021/src/interfaces/__old/Http.js';
+import IoItem from '../../../../__old/system/interfaces/IoItem';
 
 
 export const Methods = [
