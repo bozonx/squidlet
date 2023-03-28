615af2a0561ed1b625c7cc73fae4b394e5421981
Ivan <ipkozyrin@gmail.com>
2023-03-28 09:54:52 +0300

    change

--- src/packages/SystemCommonPkg/index.ts
+++ src/packages/SystemCommonPkg/index.ts
@@ -1,12 +1,12 @@
 import {PackageContext} from '../../system/package/PackageContext.js'
 import {PackageIndex} from '../../types/types.js'
-import {WsClientDriver} from '../../drivers/WsClientDriver/index.js'
-import {WsServerDriver} from '../../drivers/WsServerDriver/index.js'
+//import {WsClientDriver} from '../../drivers/WsClientDriver/index.js'
+//import {WsServerDriver} from '../../drivers/WsServerDriver/index.js'
 
 
 export function SystemCommonPkg (): PackageIndex {
   return (ctx: PackageContext) => {
-    ctx.useDriver(WsClientDriver())
-    ctx.useDriver(WsServerDriver())
+    //ctx.useDriver(WsClientDriver())
+    //ctx.useDriver(WsServerDriver())
   }
 }
--- src/sysPackages/DevSystemPack/index.ts
+++ src/sysPackages/DevSystemPack/index.ts
@@ -1,12 +1,14 @@
 import {PackageContext} from '../../system/package/PackageContext.js'
 import {PackageIndex} from '../../types/types.js'
-import {WsClientIoIndex} from '../../BasicIo/WsClientIo/index.js'
-import {WsServerIoIndex} from '../../BasicIo/WsServerIo/index.js'
+//import {WsClientIoIndex} from '../../BasicIo/WsClientIo/index.js'
+//import {WsServerIoIndex} from '../../BasicIo/WsServerIo/index.js'
+import {FilesIoIndex} from '../LinuxX86SystemPack/Ios/FilesIo.js'
 
 
 export function DevSystemPack (): PackageIndex {
   return (ctx: PackageContext) => {
-    ctx.useIo(WsClientIoIndex())
-    ctx.useIo(WsServerIoIndex())
+    ctx.useIo(FilesIoIndex)
+    //ctx.useIo(WsClientIoIndex())
+    //ctx.useIo(WsServerIoIndex())
   }
 }
