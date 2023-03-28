f973f5d046a5988321fbbf57624451c87939af67
Ivan <ipkozyrin@gmail.com>
2023-03-28 18:11:59 +0300

    add

--- src/system/files/FilesCache.ts
+++ src/system/files/FilesCache.ts
@@ -1,15 +1,53 @@
+import {clearRelPathLeft} from 'squidlet-lib'
 import {System} from '../System.js'
+import {FilesDriver} from '../../drivers/FilesDriver/FilesDriver.js'
 
 
 // TODO: файлы сразу сохранять и одновременно держать в памяти несколько секунд
 // TODO: если протухание короткое то держать только в памяти
+
+interface CacheParams {
+  // if set this id will be used
+  // if not set it will be generated
+  id?: string
+  // num in seconds when cache will be expired
+  expiredSec?: number
+  // if true cache will reset expiration on each access.
+  // if false access doesn't matter, it will be expired any way
+  expiredAfterLastAccess?: boolean
+}
+
+
 export class FilesCache {
   readonly rootDir: string
 
   private readonly system: System
 
+  private get driver(): FilesDriver {
+    return this.system.drivers.getDriver('FilesDriver')
+  }
+
+
   constructor(system: System, rootDir: string) {
     this.system = system
-    this.rootDir = rootDir
+    this.rootDir = clearRelPathLeft(rootDir)
   }
+
+
+  async getCache(idOrName: string): Promise<string | undefined> {
+    // TODO: сначала проверить в памяти - memStorage
+    // TODO: если нет то в файле
+    return
+  }
+
+  async setCache(data: string, params?: CacheParams): Promise<string> {
+    const id = params?.id || ''
+
+    // TODO: сгенерировать id
+    // TODO: сохранить в память
+    // TODO: сохранить на диск
+
+    return id
+  }
+
 }
--- src/system/files/FilesVersioned.ts
+++ src/system/files/FilesVersioned.ts
@@ -1,15 +1,127 @@
+import {clearRelPathLeft, pathJoin} from 'squidlet-lib'
 import {System} from '../System.js'
+import {StatsSimplified} from '../../types/io/FilesIoType.js'
+import {FilesDriver} from '../../drivers/FilesDriver/FilesDriver.js'
 
+
+// TODO: add versions
 
 export class FilesVersioned {
   readonly rootDir: string
 
   private readonly system: System
 
+  private get driver(): FilesDriver {
+    return this.system.drivers.getDriver('FilesDriver')
+  }
+
 
   constructor(system: System, rootDir: string) {
     this.system = system
-    this.rootDir = rootDir
+    this.rootDir = clearRelPathLeft(rootDir)
+  }
+
+
+  async appendFile(pathTo: string, data: string | Uint8Array) {
+    return this.driver.appendFile(pathJoin(this.rootDir, pathTo), data)
+  }
+
+  async mkdir(pathTo: string) {
+    return this.driver.mkdir(pathJoin(this.rootDir, pathTo))
+  }
+
+  async readDir(pathTo: string): Promise<string[]> {
+    return this.driver.readDir(pathJoin(this.rootDir, pathTo))
+  }
+
+  async readTextFile(pathTo: string): Promise<string> {
+    return this.driver.readTextFile(pathJoin(this.rootDir, pathTo))
+  }
+
+  async readBinFile(pathTo: string): Promise<Uint8Array> {
+    return this.driver.readBinFile(pathJoin(this.rootDir, pathTo))
+  }
+
+  async readlink(pathTo: string): Promise<string> {
+    return this.driver.readlink(pathJoin(this.rootDir, pathTo))
+  }
+
+  async rmdir(pathTo: string) {
+    return this.driver.rmdir(pathJoin(this.rootDir, pathTo))
+  }
+
+  async unlink(pathTo: string) {
+    return this.driver.unlink(pathJoin(this.rootDir, pathTo))
+  }
+
+  async writeFile(pathTo: string, data: string | Uint8Array) {
+    return this.driver.writeFile(pathJoin(this.rootDir, pathTo), data)
+  }
+
+  async stat(pathTo: string): Promise<StatsSimplified> {
+    return this.driver.stat(pathJoin(this.rootDir, pathTo))
+  }
+
+  async copyFiles(files: [string, string][]) {
+    return this.driver.copyFiles(files.map(([src, dest]) => {
+      return [
+        pathJoin(this.rootDir, src),
+        pathJoin(this.rootDir, dest)
+      ]
+    }))
+  }
+
+  async renameFiles(files: [string, string][]) {
+    return this.driver.copyFiles(files.map(([src, dest]) => {
+      return [
+        pathJoin(this.rootDir, src),
+        pathJoin(this.rootDir, dest)
+      ]
+    }))
+  }
+
+  async rm(pathToFileOrDir: string) {
+    return this.driver.rm(pathJoin(this.rootDir, pathToFileOrDir))
+  }
+
+  async rmRf(pathToFileOrDir: string): Promise<void> {
+    return this.driver.rmRf(pathJoin(this.rootDir, pathToFileOrDir))
+  }
+
+  async cp(src: string | string[], destDir: string): Promise<void> {
+    const fixedSrc = (typeof src === 'string')
+      ? pathJoin(this.rootDir, src)
+      : src.map((el) => pathJoin(this.rootDir, el))
+
+    return this.driver.cp(fixedSrc, pathJoin(this.rootDir, destDir))
+  }
+
+  async mv(src: string | string[], destDir: string): Promise<void> {
+    const fixedSrc = (typeof src === 'string')
+      ? pathJoin(this.rootDir, src)
+      : src.map((el) => pathJoin(this.rootDir, el))
+
+    return this.driver.mv(fixedSrc, pathJoin(this.rootDir, destDir))
+  }
+
+  async rename(pathToFileOrDir: string, newName: string): Promise<void> {
+    return this.driver.rename(pathJoin(this.rootDir, pathToFileOrDir), newName)
+  }
+
+  async isFile(pathToFile: string) {
+    return this.driver.isFile(pathJoin(this.rootDir, pathToFile))
+  }
+
+  async isExists(pathToFileOrDir: string): Promise<boolean> {
+    return this.driver.isExists(pathJoin(this.rootDir, pathToFileOrDir))
+  }
+
+  async mkDirP(pathToDir: string): Promise<void> {
+    return this.driver.mkDirP(pathJoin(this.rootDir, pathToDir))
+  }
+
+  async isFileUtf8(pathTo: string): Promise<boolean> {
+    return this.driver.isFileUtf8(pathJoin(this.rootDir, pathTo))
   }
 
 }
