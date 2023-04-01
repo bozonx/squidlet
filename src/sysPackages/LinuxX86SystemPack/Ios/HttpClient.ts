10a4c34b15647f5b819f22380b95eb66eb1e493e
Ivan <ipkozyrin@gmail.com>
2023-04-01 17:38:14 +0300

    change

--- src/sysPackages/LinuxX86SystemPack/Ios/HttpClient.ts
+++ src/sysPackages/LinuxX86SystemPack/Ios/HttpClient.ts
@@ -1,26 +1,27 @@
-import axios, {AxiosResponse} from 'axios';
+import axios, {AxiosResponse} from 'axios'
+import {HttpClientIoType} from '../../../types/io/HttpClientIoType.js'
+import {HttpRequest, HttpResponse} from '../../../types/Http.js'
 
-import {HttpClientIo} from '../../../../../../squidlet/__old/system/interfaces/io/HttpClientIoType';
-import {HttpRequest, HttpResponse} from '../../../../../../squidlet/__old/system/interfaces/Http';
 
-
-export default class HttpClient implements HttpClientIo {
+export default class HttpClient implements HttpClientIoType {
   async fetch(request: HttpRequest): Promise<HttpResponse> {
     const result: AxiosResponse = await axios({
       method: request.method,
-      headers: request.headers,
+      headers: request.headers as Record<string, any>,
       url: request.url,
       // TODO: как должен передаваться body
+      // TODO: если стоит бинарный content-type то передавать бинарно
       data: request.body,
-    });
+    })
 
     return {
       // TODO: проверить чтобы были в kebab формате
-      headers: result.headers,
+      headers: result.headers as Record<string, any>,
       status: result.status,
       // TODO: что с body - наверное надо конвертнуть Buffer в Uint если строка то оставить
+      // TODO: если стоит бинарный content-type то получать бинарно
       body: result.data,
-    };
+    }
   }
 
 }
--- src/types/io/HttpClientIoType.ts
+++ src/types/io/HttpClientIoType.ts
@@ -1,12 +1,6 @@
-import {HttpRequest, HttpResponse} from '../../../../../../../../../mnt/disk2/workspace/squidlet/__idea2021/src/interfaces/__old/Http.js';
-import IoItem from '../../../../__old/system/interfaces/IoItem';
+import {HttpRequest, HttpResponse} from '../Http.js'
 
 
-export const Methods = [
-  'fetch',
-];
-
-
-export interface HttpClientIoType extends IoItem {
-  fetch(request: HttpRequest): Promise<HttpResponse>;
+export interface HttpClientIoType {
+  fetch(request: HttpRequest): Promise<HttpResponse>
 }
