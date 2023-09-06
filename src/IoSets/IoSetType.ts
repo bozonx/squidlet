d0a4dfb452753a094c9405a7a08b63ac64332ea4
Ivan <ipkozyrin@gmail.com>
2023-09-05 22:58:52 -0300

    WIP

---
+++ src/IoSets/IoSetType.ts
@@ -0,0 +1,34 @@
+import IoItem from '../../../../../../../../mnt/disk2/workspace/squidlet/__old/system/interfaces/IoItem.js';
+
+
+export interface IoSetType {
+  /**
+   * It returns the instance of IO which was created on initialization
+   * @param ioName
+   */
+  getIo<T extends IoItem>(ioName: string): T;
+
+  /**
+   * Get all the names of platforms items
+   */
+  getNames(): string[];
+
+  /**
+   * It is called before instantiating System if set.
+   */
+  prepare?(): Promise<void>;
+
+  /**
+   * It is called at the beginning of System initialization
+   */
+  init?(): Promise<void>;
+
+
+  destroy(): Promise<void>;
+
+  // TODO: зачем ???
+  /**
+   * Require js|ts file from local disk and execute it.
+   */
+  requireLocalFile(fileName: string): Promise<any>;
+}
